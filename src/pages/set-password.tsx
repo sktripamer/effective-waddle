89c27d0d83f36dc4eccb3470984c6bf057d20a4f
sktripamer <42321951+sktripamer@users.noreply.github.com>
2021-12-02 23:25:13 -0800

    Update set-password.tsx

--- src/pages/set-password.tsx
+++ src/pages/set-password.tsx
@@ -2,18 +2,29 @@ import * as React from "react";
 
 import Layout from "../components/Layout";
 import SetPasswordForm from "../components/SetPasswordForm";
-
+const isBrowser = typeof window !== "undefined";
 export default function SetPassword() {
-  if (typeof window !== `undefined`) {
+  if (isBrowser) {
   const params = new URLSearchParams(window.location.search);
   const resetKey = params.get('key') || '';
   const login = params.get('login') || '';
-
   return (
     <Layout>
       <h1>Set Password</h1>
       <SetPasswordForm resetKey={resetKey} login={login} />
     </Layout>
   )
+  } else {
+    const params = '';
+    const resetKey = '';
+    const login = '';
+    return (
+      <Layout>
+        <h1>Set Password</h1>
+        <SetPasswordForm resetKey={resetKey} login={login} />
+      </Layout>
+    )
   }
+
+  
 }

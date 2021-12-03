e17f58b74e2c33dd1cf336854f09272c7331783b
sktripamer <42321951+sktripamer@users.noreply.github.com>
2021-12-02 23:02:52 -0800

    Update set-password.tsx

--- src/pages/set-password.tsx
+++ src/pages/set-password.tsx
@@ -5,11 +5,11 @@ import SetPasswordForm from "../components/SetPasswordForm";
 
 export default function SetPassword() {
   if (typeof window !== "undefined"){
-    const params = new URLSearchParams(window.location.search);
-  }
+  const params = new URLSearchParams(window.location.search);
+  
   const resetKey = params.get('key') || '';
   const login = params.get('login') || '';
-
+  }
   return (
     <Layout>
       <h1>Set Password</h1>

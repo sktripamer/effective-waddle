f255678817d62aeb8d998a37abb08686a4016de0
sktripamer <42321951+sktripamer@users.noreply.github.com>
2021-12-17 08:21:38 -0800

    Update friends-email.js

--- src/api/friends-email.js
+++ src/api/friends-email.js
@@ -2,29 +2,44 @@ export default async function handler(req, res) {
     try {
 const emails = req.body.split('@@')
 
-    const bodied ={
-        "acf": {
-            "email1": emails[0],
-            "email2": emails[1],
-            "email3": emails[2],
-              }
-      } 
-      
 
-
-    const modify = await  fetch('https://portal.revrevdev.xyz/wp-json/wp/v2/users/' + emails[3], {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json;charset=UTF-8',
-          'Authorization': "Basic YWRtaW46YWZhOSBvNTJSIG9uNWsgUVdHTCA4M0dMIHVlR3I=",
-        },
-         body: JSON.stringify(bodied),
       
-      })
-      const customer = (await modify.json());
-   return res.status(200).json({customer})
+const custobj = await getCustomerObj(emails[0],emails[1],emails[2],emails[3]);
+
+   return res.status(200).json({custobj})
     } catch (e) {
         return res.status(500).json({e})
     }
 
 }
+
+
+async function getCustomerObj(email1, email2, email3, id) {
+    try {
+
+        const bodied ={
+            "acf": {
+                "email1": emails[0],
+                "email2": emails[1],
+                "email3": emails[2],
+                  }
+          } 
+      // Retrieve email and username of the currently logged in user.
+      // getUserFromDB() is *your* implemention of getting user info from the DB
+
+      const request = await fetch('https://portal.revrevdev.xyz/wp-json/wp/v2/users/' + emails[3], {
+        method: 'POST',
+        headers: {
+            'Authorization': "Basic YWRtaW46YWZhOSBvNTJSIG9uNWsgUVdHTCA4M0dMIHVlR3I="
+          },
+          body: JSON.stringify(bodied),
+      });
+      const customerobj = (await request.json());
+      // Update your user in DB to store the customerID
+      // updateUserInDB() is *your* implementation of updating a user in the DB
+      return customerobj;
+    } catch (error) {
+
+      return null;
+    }
+  }

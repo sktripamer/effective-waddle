6bd3dd03ee7ed9e660ebe1fce4d9b80fa166819a
sktripamer <42321951+sktripamer@users.noreply.github.com>
2021-12-20 17:57:29 -0800

    Update friends-email.js

--- src/api/friends-email.js
+++ src/api/friends-email.js
@@ -4,8 +4,8 @@ export default async function postNewPersonHandler(req, res) {
    // const emails = req.body.split('@@')
     
   // POST data to an authenticated API
-  try { 
-    const decoded = jwt.verify('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvcG9ydGFsLnJldnJldmRldi54eXoiLCJpYXQiOjE2NDAwNTEwOTEsIm5iZiI6MTY0MDA1MTA5MSwiZXhwIjoxNjQwMDUxMzkxLCJkYXRhIjp7InVzZXIiOnsiaWQiOiIzNSJ9fX0.2pmJC8LqI8omzsDSeePGnkzuAc4i4igheL6flsJcrG4', 'sAd}aBA(Lm{&Z))WK|whV|nBU=zf6J^hshgMMfkU;Hmc Ky/5mRx2G^4/LsE-q`|',{ ignoreExpiration: true});
+ // try { 
+  //  const decoded = jwt.verify('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvcG9ydGFsLnJldnJldmRldi54eXoiLCJpYXQiOjE2NDAwNTEwOTEsIm5iZiI6MTY0MDA1MTA5MSwiZXhwIjoxNjQwMDUxMzkxLCJkYXRhIjp7InVzZXIiOnsiaWQiOiIzNSJ9fX0.2pmJC8LqI8omzsDSeePGnkzuAc4i4igheL6flsJcrG4', 'sAd}aBA(Lm{&Z))WK|whV|nBU=zf6J^hshgMMfkU;Hmc Ky/5mRx2G^4/LsE-q`|',{ ignoreExpiration: true});
     
   const url = 'https://portal.revrevdev.xyz/wp-json/wp/v2/users/35'// + decoded.data.user.id
   
@@ -33,8 +33,8 @@ export default async function postNewPersonHandler(req, res) {
   } catch (error) {
     res.status(500).send(error)
   }
-   }
-   catch (ex) {   res.status(500).send(ex.message); }
+  // }
+  // catch (ex) {   res.status(500).send(ex.message); }
    
 
 }

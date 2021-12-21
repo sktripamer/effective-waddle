bd8a3db67fd716b99f2ad7e29ed90f1f1701a566
sktripamer <42321951+sktripamer@users.noreply.github.com>
2021-12-20 17:50:00 -0800

    Update friends-email.js

--- src/api/friends-email.js
+++ src/api/friends-email.js
@@ -1,13 +1,13 @@
 import fetch from "node-fetch"
 import jwt from 'jsonwebtoken'
 export default async function postNewPersonHandler(req, res) {
-    const emails = req.body.split('@@')
+   // const emails = req.body.split('@@')
     
   // POST data to an authenticated API
-  try {
-    const decoded = jwt.verify(emails[3], 'sAd}aBA(Lm{&Z))WK|whV|nBU=zf6J^hshgMMfkU;Hmc Ky/5mRx2G^4/LsE-q`|',{ ignoreExpiration: true});
+  try { 
+    const decoded = jwt.verify('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvcG9ydGFsLnJldnJldmRldi54eXoiLCJpYXQiOjE2NDAwNTEwOTEsIm5iZiI6MTY0MDA1MTA5MSwiZXhwIjoxNjQwMDUxMzkxLCJkYXRhIjp7InVzZXIiOnsiaWQiOiIzNSJ9fX0.2pmJC8LqI8omzsDSeePGnkzuAc4i4igheL6flsJcrG4', 'sAd}aBA(Lm{&Z))WK|whV|nBU=zf6J^hshgMMfkU;Hmc Ky/5mRx2G^4/LsE-q`|',{ ignoreExpiration: true});
     
-  const url = 'https://portal.revrevdev.xyz/wp-json/wp/v2/users/' + decoded.data.user.id
+  const url = 'https://portal.revrevdev.xyz/wp-json/wp/v2/users/35'// + decoded.data.user.id
   
   const headers = {
     "Content-Type": "application/json",
@@ -16,9 +16,9 @@ export default async function postNewPersonHandler(req, res) {
   
   const data = {
     "acf": {
-        "email1": emails[0],
-        "email2": emails[1],
-        "email3": emails[2],
+        "email1": 'test123@gmail.com',
+        "email2": 'test123@gmail.com',
+        "email3": 'test123@gmail.com',
           }
   }
   try {

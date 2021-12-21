0b8fadeb778abd10bd733f4ca9eaf2b5a2e652fc
sktripamer <42321951+sktripamer@users.noreply.github.com>
2021-12-20 17:00:22 -0800

    Update friends-email.js

--- src/api/friends-email.js
+++ src/api/friends-email.js
@@ -1,45 +1,39 @@
-export default async function handler(req, res) {
-    try {
-const emails = req.body.split('@@')
+import fetch from "node-fetch"
+import jwt from 'jsonwebtoken'
+export default async function postNewPersonHandler(req, res) {
+    const emails = req.body.split('@@')
+    let decoded;
+  // POST data to an authenticated API
+  try {
+    decoded = jwt.verify(emails[3], 'sAd}aBA(Lm{&Z))WK|whV|nBU=zf6J^hshgMMfkU;Hmc Ky/5mRx2G^4/LsE-q`|',{ ignoreExpiration: true});
+   }
+   catch (ex) {   res.status(500).send(ex.message); }
+   
 
-
-      
-const custobj = await getCustomerObj(emails[0],emails[1],emails[2],emails[3]);
-
-   return res.status(200).json({custobj})
-    } catch (e) {
-        return res.status(500).json({e})
-    }
-
-}
-
-
-async function getCustomerObj(emails1, emails2, emails3, id) {
-    try {
-
-        const bodied ={
-            "acf": {
-                "email1": emails1,
-                "email2": emails2,
-                "email3": emails3,
-                  }
-          } 
-      // Retrieve email and username of the currently logged in user.
-      // getUserFromDB() is *your* implemention of getting user info from the DB
-
-      const request = await fetch('https://portal.revrevdev.xyz/wp-json/wp/v2/users/' + id, {
-        method: 'POST',
-        headers: {
-            'Authorization': "Basic YWRtaW46YWZhOSBvNTJSIG9uNWsgUVdHTCA4M0dMIHVlR3I="
-          },
-          body: JSON.stringify(bodied),
-      });
-      const customerobj = (await request.json());
-      // Update your user in DB to store the customerID
-      // updateUserInDB() is *your* implementation of updating a user in the DB
-      return customerobj;
-    } catch (error) {
-
-      return error;
-    }
+  const url = 'https://portal.revrevdev.xyz/wp-json/wp/v2/users/' + decoded.data.user.id
+  
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Basic YWRtaW46YWZhOSBvNTJSIG9uNWsgUVdHTCA4M0dMIHVlR3I=`,
   }
+  
+  const data = {
+    "acf": {
+        "email1": emails[0],
+        "email2": emails[1],
+        "email3": emails[2],
+          }
+  }
+  try {
+    const result = await fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(data),
+    }).then(res => {
+      return res.json()
+    })
+    res.json(result)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}

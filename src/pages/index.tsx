bb2dc37e4c277d5c09808123188ccb69b6af486d
sktripamer <42321951+sktripamer@users.noreply.github.com>
2021-12-20 20:39:26 -0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Layout from "../components/Layout";
 import CustomizableVideoPlayer from '@folly-systems/custom-react-player'
 import useAuth from "../hooks/useAuth";
@@ -9,6 +9,10 @@ import SignUpForm from "../components/SignUpForm";
 //import RegisterOpt from "../components/RegisterOpt";
 import UnAuthContent from "../components/UnAuthContent";
 //import LoginVerify from "../components/LoginVerify";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import InputField2 from '../components/inputfield';
+
+
 import { useBetween } from "use-between";
 import MessageAlert from "../components/MessageAlert";
 import * as Yup from "yup";
@@ -102,6 +106,23 @@ const step3verify = () => {
   if (typeof window !== "undefined") {
 
     let authTokenData = localStorage.getItem("s3");
+
+    if (!isEmpty(authTokenData)) {
+
+       
+       return true
+     
+      
+    } else {
+      return false
+    }
+  } // // this will fire only when loadDataOnlyOnce-reference changes
+}
+
+const step4verify = () => {
+  if (typeof window !== "undefined") {
+
+    let authTokenData = localStorage.getItem("s4");
 
     if (!isEmpty(authTokenData)) {
 
@@ -711,6 +732,233 @@ const StepThree = () => {
 
 }
 
+const StepFour = () => {
+  const {playing, setPlaying } = useBetween(useShareableState);
+  const {videoStatus, setVideoStatus } = useBetween(useShareableState);
+  const {videoTime, setVideoTime } = useBetween(useShareableState);
+
+
+//   function handleSubmit(e) {
+//     e.preventDefault();
+ 
+
+//     localStorage.removeItem("s4")
+//     localStorage.setItem("s5", "y")
+//        setVideoTime(3)
+//        setPlaying({status: true, time: 8.99, speed: 1})
+//        setVideoStatus(0)
+//   }
+
+
+
+//   return (
+// <div className="sharesection">
+  
+// </div>
+//  )
+
+
+const [succeeded, setSucceeded] = useState(false);
+const [error, setError] = useState(null);
+const [processing, setProcessing] = useState("");
+const [disabled, setDisabled] = useState(true);
+const [clientSecret, setClientSecret] = useState("");
+const nameForm = useRef(null);
+const [successMessage, setSuccessMessage] = useState("");
+const [showAlertBar, setShowAlertBar] = useState(true);
+const [status, setStatus] = React.useState(0) // 0: no show, 1: show yes, 2: show no.
+const stripe = useStripe();
+const elements = useElements();
+
+// useEffect(() => {
+//   window
+//     .fetch("/api/payment-intent", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//            amount: 500,
+//       }),
+//     })
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((body) => {
+//       console.log(body)
+//       setClientSecret(body.body.client_secret);
+//     });
+// }, []);
+
+const radioHandler = (status) => {
+  setStatus(status);
+};
+
+
+async function createIntent() {
+  try {
+    // Retrieve email and username of the currently logged in user.
+    // getUserFromDB() is *your* implemention of getting user info from the DB
+    const form = nameForm.current
+    const email = form['firstname'].value
+    const request = await fetch('/api/payment-intent', {
+      method: 'POST',
+      body: email,
+    });
+    const intent = (await request.json());
+    // Update your user in DB to store the customerID
+    // updateUserInDB() is *your* implementation of updating a user in the DB
+    return intent;
+  } catch (error) {
+    console.log('Failed to create intent');
+    console.log(error);
+    return null;
+  }
+}
+
+
+// async function getCustomerObj() {
+//   try {
+//     // Retrieve email and username of the currently logged in user.
+//     // getUserFromDB() is *your* implemention of getting user info from the DB
+
+//     const request = await fetch('/api/retrieve-customer', {
+//       method: 'POST',
+//       body: 'a',
+//     });
+//     const customerobj = (await request.json());
+//     // Update your user in DB to store the customerID
+//     // updateUserInDB() is *your* implementation of updating a user in the DB
+//     return customerobj;
+//   } catch (error) {
+//     console.log('Failed to get customer');
+//     console.log(error);
+//     return null;
+//   }
+// }
+
+// fetch(`/api/payment-intent`, {
+//   method: "POST",
+//   body: JSON.stringify({
+//       amount: 500,
+//     }),
+//   headers: {
+//       "content-type": `application/json`,
+//      },
+//   })
+//   .then(res => res.json())
+//   .then(body => {
+//     console.log(body)
+//     stripe.confirmCardPayment(body.body.client_secret, {
+//       payment_method: {
+//         card: elements.getElement(CardElement),
+//         billing_details: {
+//           email: "coolio123@gmail.com",
+//         },
+//       },
+//     });
+//   })
+
+const cardStyle = {
+  style: {
+    base: {
+      color: "#32325d",
+      fontFamily: "Arial, sans-serif",
+      fontSmoothing: "antialiased",
+      fontSize: "16px",
+      "::placeholder": {
+        color: "#32325d",
+      },
+    },
+    invalid: {
+      color: "#fa755a",
+      iconColor: "#fa755a",
+    },
+  },
+};
+
+// const getCustomer = async (ev) => {
+//   ev.preventDefault();
+//   const custobj = await getCustomerObj();
+//   console.log(custobj)
+// }
+
+
+const handleChange = async (event) => {
+  setDisabled(event.empty);
+  setError(event.error ? event.error.message : "");
+};
+
+const handleSubmit = async (ev) => {
+  ev.preventDefault();
+  setProcessing(true);
+  const intent = await createIntent();
+  console.log(intent)
+  //setClientSecret(intent.body.client_secret);
+  const payload = await stripe.confirmCardPayment(intent.body.client_secret, {
+    payment_method: {
+      card: elements.getElement(CardElement),
+    },
+  });
+
+  if (payload.error) {
+    setError(`Payment failed ${payload.error.message}`);
+    setProcessing(false);
+  } else {
+    setError(null);
+    setProcessing(false);
+    setSucceeded(true);
+        localStorage.removeItem("s4")
+    localStorage.setItem("s5", "y")
+       setVideoTime(4)
+       setPlaying({status: true, time: 11.99, speed: 1})
+       setVideoStatus(0)
+    setSuccessMessage("first payment complete");
+  }
+};
+
+
+
+
+return (
+  <div className='payment'>
+  <form id="payment-form" ref={nameForm} onSubmit={handleSubmit}>
+    <InputField2 label={'email'} name={'firstname'}/>
+    <CardElement
+      id="card-element"
+      options={cardStyle}
+      onChange={handleChange}
+    />
+    <button disabled={processing || disabled || succeeded} id="submit">
+      <span id="button-text">
+        {processing ? (
+          <div className="spinner" id="spinner"></div>
+        ) : (
+          "Pay now"
+        )}
+      </span>
+    </button>
+    {error && (
+      <div className="card-error" role="alert">
+        {error}
+      </div>
+    )}
+    <p className={succeeded ? "result-message" : "result-message hidden"}>
+      Payment succeeded, see the result in your
+      <a href={`https://dashboard.stripe.com/test/payments`}>
+        Stripe dashboard.
+      </a>
+      Refresh the page to pay again.
+    </p>
+  </form>
+
+
+  </div>
+);
+
+}
+
+
 
 const IndexPage = () => {
 
@@ -829,6 +1077,11 @@ const IndexPage = () => {
         setPlaying({status: false, time: 8.99, speed: speed})
         setVideoStatus(3)
       }
+      if (currentTime > 12  && step4verify()===true) {
+        console.log('yes3')
+        setPlaying({status: false, time: 11.99, speed: speed})
+        setVideoStatus(4)
+      }
       console.log(currentTime)
       console.log(isPlaying)
       console.log(loggedIn)
@@ -869,6 +1122,14 @@ const IndexPage = () => {
                 
 
               <StepThree />  
+    
+              )
+            : ""}
+              {4 == videoStatus
+            ? showAlertBar && (
+                
+
+              <StepFour />  
     
               )
             : ""}

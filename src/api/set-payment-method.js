68379a02d93e66020037a9787bd6bb6f495af21d
sktripamer <42321951+sktripamer@users.noreply.github.com>
2021-12-22 11:37:38 -0800

    Create set-payment-method.js

---
+++ src/api/set-payment-method.js
@@ -0,0 +1,69 @@
+import fetch from "node-fetch"
+import jwt from 'jsonwebtoken'
+import axios from 'axios'
+const setMethod = async (req, res) => {
+
+
+try {
+ 
+   
+        //return decoded.data.user.user_email;
+        //res.status(200).json(decoded.data.user.user_email)
+        const emails = req.body.split('@@')
+        jwt.verify(emails[1], process.env.JWT_SECRET,{ ignoreExpiration: true}, async function(err, decoded) {
+         const customerID = await createCustomer(emails[0]);
+         const paymentIntent = await createIntent(customerID, decoded.data.user.id);
+         
+        await res.status(200).json({body:paymentIntent});
+          
+    });
+  } catch (e) {
+    res.json({body: 'error ' + e})
+  }
+
+}
+
+const createCustomer = async (cID) => {
+
+    try {
+      // get payment method
+      const customerID = await stripe.customers.listPaymentMethods(
+        cID,
+        {type: 'card'}
+      );
+      
+      return customerID.customer.data[0].id;
+  
+    } catch (error) {
+      return error;
+    }
+    
+  }
+
+  const createIntent = async (cID, uID) => {
+    const data = {
+        "acf": {
+            "payment_method": cID,
+              }
+      }
+      let axiosConfig = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Basic ` + process.env.REST_SECRET,
+        }
+      };
+      
+      axios.post('https://portal.revrevdev.xyz/wp-json/wp/v2/users/' + uID, JSON.stringify(data), axiosConfig)
+    try {
+    
+      return 'success';
+    } catch (error) {
+        return error;
+    }
+    
+  }
+  
+
+export default setMethod
+
+

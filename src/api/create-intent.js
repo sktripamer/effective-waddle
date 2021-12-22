3088ddb32755c4019eee3831a607ea545982a7e7
sktripamer <42321951+sktripamer@users.noreply.github.com>
2021-12-22 05:53:54 -0800

    Update create-intent.js

--- src/api/create-intent.js
+++ src/api/create-intent.js
@@ -14,7 +14,9 @@ try {
     const emails = req.body.split('@@')
     jwt.verify(emails[1], process.env.JWT_SECRET,{ ignoreExpiration: true}, function(err, decoded) {
         //return decoded.data.user.user_email;
-        res.status(200).json(decoded.data.user.user_email)
+        //res.status(200).json(decoded.data.user.user_email)
+         const customerID = await createCustomer(decoded.data.user.user_email);
+         res.status(200).json(customerID);
       });
 //   const paymentIntent = await stripe.paymentIntents.create({
 //     amount: 500,

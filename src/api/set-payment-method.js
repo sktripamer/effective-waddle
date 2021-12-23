0f8ca768fc9dc2a4a1cfa0ee1a67524f53f5e422
sktripamer <42321951+sktripamer@users.noreply.github.com>
2021-12-22 22:15:00 -0800

    Update set-payment-method.js

--- src/api/set-payment-method.js
+++ src/api/set-payment-method.js
@@ -17,9 +17,9 @@ try {
         // );
         jwt.verify(emails[1], process.env.JWT_SECRET,{ ignoreExpiration: true}, async function(err, decoded) {
         const customerID = await createCustomer(emails[0]);
-        const paymentIntent = await createIntent(customerID, decoded.data.user.id);
+        //const paymentIntent = await createIntent(customerID, decoded.data.user.id);
          
-       await res.status(200).json({body:paymentIntent});
+       await res.status(200).json({body:JSON.stringify(customerID)});
       return res.status(200).json({customerID})
    });
   } catch (e) {
@@ -37,7 +37,7 @@ const createCustomer = async (cID) => {
         {type: 'card'}
       );
       
-      return customerID.customer.data[0].id;
+      return customerID;
      //return customerID;
   
     } catch (error) {

c3daf6debb51765073a80fc759b3bba6ff47666b
sktripamer <42321951+sktripamer@users.noreply.github.com>
2021-12-23 19:16:23 -0800

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
 		  module: {
 			rules: [
 			  {
-				test: /custom-react-player/,
+				test: /@vime/,
 				use: loaders.null(),
 			  },
 			],

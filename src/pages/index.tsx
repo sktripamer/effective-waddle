45e9bd4e07d10f222fa3936474d0b6968084db30
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-01-11 00:14:23 -0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -104,6 +104,34 @@
     };
   };
 
+  function formatPhoneNumber(value) {
+    // if input value is falsy eg if the user deletes the input, then just return
+    if (!value) return value;
+  
+    // clean the input for any non-digit values.
+    const phoneNumber = value.replace(/[^\d]/g, "");
+  
+    // phoneNumberLength is used to know when to apply our formatting for the phone number
+    const phoneNumberLength = phoneNumber.length;
+  
+    // we need to return the value with no formatting if its less then four digits
+    // this is to avoid weird behavior that occurs if you  format the area code to early
+    if (phoneNumberLength < 4) return phoneNumber;
+  
+    // if phoneNumberLength is greater than 4 and less the 7 we start to return
+    // the formatted number
+    if (phoneNumberLength < 7) {
+      return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
+    }
+  
+    // finally, if the phoneNumberLength is greater then seven, we add the last
+    // bit of formatting and return it.
+    return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
+      3,
+      6
+    )}-${phoneNumber.slice(6, 10)}`;
+  }
+
   const LoginVerify = () => {
     if (typeof window !== "undefined") {
 
@@ -282,8 +310,13 @@
     const {videoTime, setVideoTime } = useBetween(useShareableState);
     const { videoDetails, setVideoDetails } =  useBetween(useShareableState);
     // Check if the user is validated already.
-
-
+    const [inputValue, setInputValue] = useState("");
+    const handleInput = (e) => {
+      // this is where we'll call the phoneNumberFormatter function
+      const formattedPhoneNumber = formatPhoneNumber(e.target.value);
+      // we'll set the input value using our setInputValue
+      setInputValue(formattedPhoneNumber);
+    };
     /**
      * Hide the Status bar on cross button click.
      */
@@ -514,7 +547,7 @@
             {/* Second Step */}
             <FormikStep>
             <div class="input-wrap">
-              <InputField placeholder="Your Phone" name="phonefield" label="Phone" />
+              <InputField onChange={(e) => handleInput(e)} value={inputValue} placeholder="Your Phone" name="phonefield" label="Phone" />
               </div>
             </FormikStep>
           </FormikStepper>

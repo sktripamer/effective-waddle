df6677e0b808cc9e411e53c4c4e887da49ed70ef
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-01-16 05:15:42 -0800

    Update get-payment-info.js

--- src/api/get-payment-info.js
+++ src/api/get-payment-info.js
@@ -6,10 +6,10 @@ const validateJWT = async (req, res) => {
 //validates JWT and gets user ID from payload data. then passes to the next function to get customerID and paymentID from stripe.
     try {
             jwt.verify(req.body, process.env.JWT_SECRET,{ ignoreExpiration: true}, async function(err, decoded) {
-            const customerID = await getPaymentID(decoded.data.user.id);
+           // const customerID = await getPaymentID(decoded.data.user.id);
             //const paymentIntent = await createIntent(customerID, decoded.data.user.id);
              
-          return res.status(200).json(customerID);
+          return res.status(200).json(decoded.data.user.id);
          // return res.status(200).json({customerID})
        });
       } catch (e) {

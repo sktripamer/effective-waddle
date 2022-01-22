a58ce252b1ca7e3b60cc8cf67f5aecb24fff928d
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-01-22 08:15:49 -0800

    Update user-exists.js

--- src/api/user-exists.js
+++ src/api/user-exists.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const userExists = async (req, res) => {
 
      const responser = await axios.get('https://portal.revrevdev.xyz/wp-json/tv5hoq1wzdbgy0y6y4s8krvqvp759kc03vu7snia/user-exists?email=' + encodeURIComponent(req.body)).then(resp => {    
-       return resp;
+       return resp.data;
     });
     return res.status(200).json(responser)
 }

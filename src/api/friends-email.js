8ed346ca37fa95c967395e126b408046a93ec31f
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-01-22 20:52:32 -0800

    Update friends-email.js

--- src/api/friends-email.js
+++ src/api/friends-email.js
@@ -1,12 +1,19 @@
 import fetch from "node-fetch"
 import jwt from 'jsonwebtoken'
 import axios from 'axios'
+
+function validateEmail(email) 
+    {
+        var re =  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
 const saveFriends = async (req, res) => {
-   // const emails = req.body.split('@@')
+//    const emails = req.body.split('@@')
     
-  // POST data to an authenticated API
- // try { 
-  //  const decoded = jwt.verify('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvcG9ydGFsLnJldnJldmRldi54eXoiLCJpYXQiOjE2NDAwNTEwOTEsIm5iZiI6MTY0MDA1MTA5MSwiZXhwIjoxNjQwMDUxMzkxLCJkYXRhIjp7InVzZXIiOnsiaWQiOiIzNSJ9fX0.2pmJC8LqI8omzsDSeePGnkzuAc4i4igheL6flsJcrG4', 'sAd}aBA(Lm{&Z))WK|whV|nBU=zf6J^hshgMMfkU;Hmc Ky/5mRx2G^4/LsE-q`|',{ ignoreExpiration: true});
+//   POST data to an authenticated API
+//  try { 
+//    const decoded = jwt.verify('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvcG9ydGFsLnJldnJldmRldi54eXoiLCJpYXQiOjE2NDAwNTEwOTEsIm5iZiI6MTY0MDA1MTA5MSwiZXhwIjoxNjQwMDUxMzkxLCJkYXRhIjp7InVzZXIiOnsiaWQiOiIzNSJ9fX0.2pmJC8LqI8omzsDSeePGnkzuAc4i4igheL6flsJcrG4', 'sAd}aBA(Lm{&Z))WK|whV|nBU=zf6J^hshgMMfkU;Hmc Ky/5mRx2G^4/LsE-q`|',{ ignoreExpiration: true});
     
 //   const url = 'https://portal.revrevdev.xyz/wp-json/wp/v2/users/35'// + decoded.data.user.id
   
@@ -44,13 +51,20 @@ try {
   } catch (e) {
     res.json({body: 'error ' + e})
   }
-  
-
 
 }
 
 async function getCustomer(token) {
   const emails = token.split('@@')
+  if (!validateEmail(emails[0])) {
+    emails[0] = '';
+  }
+  if (!validateEmail(emails[1])) {
+    emails[1] = '';
+  }
+  if (!validateEmail(emails[2])) {
+    emails[2] = '';
+  }
     const data = {
         "acf": {
             "email1": emails[0],

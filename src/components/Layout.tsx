aebc60d9524caf89e6175d1e0461972a55ee9f1b
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-01-25 05:49:48 -0800

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
 import { ReactNode } from "react";
-
+import { Helmet } from 'react-helmet';
 import Header from "./Header";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
+          <Helmet>
+        <html lang="en" />
+        <meta name="icon" href="../images/favicon.ico" />
+      </Helmet>
       <title>revrevdev</title>
+
       <main>{children}</main>
     </>
   );

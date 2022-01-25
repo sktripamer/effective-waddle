83e1b4882cce3156786b922f80a8910f4444ad0c
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-01-25 07:29:41 -0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1789,8 +1789,7 @@ const drawNoContent = () => {
               : ""}
                 {4 == videoStatus
               ? showAlertBar && (
-                  
-                <Elements stripe={promise}>
+                <Elements stripe={loadStripe('pk_test_51Jr6IuEIi9OXKxaBdi4aBOlRU6DgoMcQQNgDCOLo1p8TZDy29xR5tKWHP5C02bF7kKHpkWKq9DI9OCzClVtj8zi500XedIOBD3')}>
                 <StepFour />  
                 </Elements>
                 )
@@ -1798,8 +1797,8 @@ const drawNoContent = () => {
                {5 == videoStatus
               ? showAlertBar && (
                   
-              
-                <Elements stripe={promise}>
+                
+                <Elements stripe={loadStripe('pk_test_51Jr6IuEIi9OXKxaBdi4aBOlRU6DgoMcQQNgDCOLo1p8TZDy29xR5tKWHP5C02bF7kKHpkWKq9DI9OCzClVtj8zi500XedIOBD3')}>
                 <StepFive />  
                 </Elements>
               

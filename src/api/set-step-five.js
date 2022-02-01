ba861f16f544dc56761b9c9785988404dacb7254
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-02-01 12:35:00 -0800

    Update set-step-five.js

--- src/api/set-step-five.js
+++ src/api/set-step-five.js
@@ -1,21 +1,31 @@
 import jwt from 'jsonwebtoken'
 import axios from 'axios'
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 let parsedVar;
 const validateJWT = async (req, res) => {
   parsedVar = req.body
 //validates JWT and gets user ID from payload data. then passes to the next function to get customerID and paymentID from stripe.
-    try {
-            jwt.verify(parsedVar.token, process.env.JWT_SECRET,{ ignoreExpiration: true}, async function(err, decoded) {
-            const customerID = await getPaymentID(decoded.data.user.id);
-           // const paymentMethod = await getPaymentMethod(customerID);
-            //const paymentIntent = await createIntent(customerID, decoded.data.user.id);
-             
-         // return res.status(200).json(decoded.data.user.id);
-          return res.status(200).json({customerID})
-       });
-      } catch (e) {
-        res.json({body: 'error ' + e})
-      } 
+const paymentIntent = await stripe.paymentIntents.retrieve(
+  parsedVar.transactionid
+);
+
+if (paymentIntent.status === "succeeded") {
+  try {
+    jwt.verify(parsedVar.token, process.env.JWT_SECRET,{ ignoreExpiration: true}, async function(err, decoded) {
+    const customerID = await getPaymentID(decoded.data.user.id);
+   // const paymentMethod = await getPaymentMethod(customerID);
+    //const paymentIntent = await createIntent(customerID, decoded.data.user.id);
+     
+ // return res.status(200).json(decoded.data.user.id);
+  return res.status(200).json({customerID})
+});
+} catch (e) {
+res.json({body: 'error ' + e})
+} 
+} else {
+  return res.status(400);
+}
+
 }
 
 

54260e2a8ce99bfdf4c8a01aec0b9c6b52ee5123
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-02-23 09:04:52 -0800

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -19,33 +19,6 @@ export default function Layout({ children }: { children: ReactNode }) {
       <title>revrevdev</title>
 
       <main>{children}</main>
-      
-      import * as React from "react";
-import { ReactNode } from "react";
-import { Helmet } from 'react-helmet';
-import Header from "./Header";
-
-export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <>
-          <Helmet>
-        <html lang="en" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-<link rel="manifest" href="/site.webmanifest"/>
-<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
-<meta name="msapplication-TileColor" content="#da532c"/>
-<meta name="theme-color" content="#ffffff"/>
-      </Helmet>
-      <title>revrevdev</title>
-
-      <main>{children}</main>
-
-    </>
-  );
-}
-
     </>
   );
 }

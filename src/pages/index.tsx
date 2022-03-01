808e511a9c4da5fd0251d324026bca7f17d22b75
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-03-01 14:20:25 -0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2497,27 +2497,27 @@
         <div class="hello-list-number-cont">
         <div class="hello-list-number">1</div>
         </div>
-        <div class="hello-list-heading">Lorum Ipsum</div>
+        <div class="hello-list-heading">Lorum Ipsum  <div class="hello-list-subheading">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div></div>
       </div>
-        <div class="hello-list-subheading">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+       
     </div>
     <div class="hello-list-item h-second">
       <div class="hello-list-title">
         <div class="hello-list-number-cont">
         <div class="hello-list-number">2</div>
         </div>
-        <div class="hello-list-heading">Lorum Ipsum</div>
+        <div class="hello-list-heading">Lorum Ipsum  <div class="hello-list-subheading">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div></div>
       </div>
-        <div class="hello-list-subheading">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+      
     </div>
     <div class="hello-list-item h-third">
       <div class="hello-list-title">
         <div class="hello-list-number-cont">
         <div class="hello-list-number">3</div>
       </div>
-        <div class="hello-list-heading">Lorum Ipsum</div>
+        <div class="hello-list-heading">Lorum Ipsum  <div class="hello-list-subheading">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div></div>
       </div>
-        <div class="hello-list-subheading">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+      
     </div>
   </div>
 

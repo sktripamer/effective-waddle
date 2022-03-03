38c47d338046804eb623a5dbab6fbd4f890c5e23
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-03-02 21:04:16 -0800

    Create say-hello-list.tsx

---
+++ src/components/say-hello-list.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+
+export default function sayHelloList() {
+  return (
+    <div id='say-hello-list-container'>
+    <div class="hello-list-item h-first">
+      <div class="hello-list-title">
+        <div class="hello-list-number-cont">
+        <div class="hello-list-number">1</div>
+        </div>
+        <div class="hello-list-heading">Lorum Ipsum  <div class="hello-list-subheading">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div></div>
+      </div>
+       
+    </div>
+    <div class="hello-list-item h-second">
+      <div class="hello-list-title">
+        <div class="hello-list-number-cont">
+        <div class="hello-list-number">2</div>
+        </div>
+        <div class="hello-list-heading">Lorum Ipsum  <div class="hello-list-subheading">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div></div>
+      </div>
+      
+    </div>
+    <div class="hello-list-item h-third">
+      <div class="hello-list-title">
+        <div class="hello-list-number-cont">
+        <div class="hello-list-number">3</div>
+      </div>
+        <div class="hello-list-heading">Lorum Ipsum  <div class="hello-list-subheading">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div></div>
+      </div>
+      
+    </div>
+  </div>
+  );
+}

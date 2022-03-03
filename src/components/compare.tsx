64ccbba2a9376b1facf91a3050dd9d1e2792d99a
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-03-02 22:23:29 -0800

    Update compare.tsx

--- src/components/compare.tsx
+++ src/components/compare.tsx
@@ -10,17 +10,32 @@ export default function CompareList() {
     </div>
 
     <div class='compare-text-container'>
-        <div class='compare-today-container'>
-            <div class="compare-today-subtext">Lorem ipsum dolor sit amet</div>
-            <div class="compare-today-subtext">Lorem ipsum dolor sit amet</div>
-            <div class="compare-today-subtext">Lorem ipsum dolor sit amet</div>
-            <div class="compare-today-subtext">Lorem ipsum dolor sit amet</div>
+        <div class='compare-today-container'> 
+            <div class="compare-today-subtext">Getting Minimized</div>
+            <div class="compare-today-subtext">Choosing a Monotonous Lifestyle</div>
+            <div class="compare-today-subtext">Accepting An Underappreciated Work-life</div>
+            <div class="compare-today-subtext">Selling Your Talents at a Wholesale Discount</div>
+            <div class="compare-today-subtext">Working Everyday for Money</div>
+            <div class="compare-today-subtext">Losing Under the Laws of Addition</div>
+            <div class="compare-today-subtext">Only Cutting Expenses to “Make Money”</div>
+            <div class="compare-today-subtext">Settling for a Stagnant Career</div>
+            <div class="compare-today-subtext">Renting Your Time to Your Employer</div>
+            <div class="compare-today-subtext">Doing the Same Work Everyday</div>
+
         </div>
         <div class='compare-tomorrow-container'>
-            <div class="compare-tomorrow-subtext">Lorem ipsum dolor sit amet</div>
-            <div class="compare-tomorrow-subtext">Lorem ipsum dolor sit amet</div>
-            <div class="compare-tomorrow-subtext">Lorem ipsum dolor sit amet</div>
-            <div class="compare-tomorrow-subtext">Lorem ipsum dolor sit amet</div>
+            <div class="compare-tomorrow-subtext">Getting Maximized</div>
+            <div class="compare-tomorrow-subtext">Creating a Meaningful Life</div>
+            <div class="compare-tomorrow-subtext">Recruiting, Hiring, &amp; Blessing Those You Lead</div>
+            <div class="compare-tomorrow-subtext">Selling Your Skill Sets at a Retail Premium</div>
+            <div class="compare-tomorrow-subtext">Generating Multiple Revenue Streams</div>
+            <div class="compare-tomorrow-subtext">Allowing Money to Work for You</div>
+            <div class="compare-tomorrow-subtext">Winning With the Laws of Multiplication</div>
+            <div class="compare-tomorrow-subtext">Increasing Revenue to Actually Make Money</div>
+            <div class="compare-tomorrow-subtext">Striving to Lead a Meaningful Life</div>
+            <div class="compare-tomorrow-subtext">Owning Your Time as a Business Owner</div>
+            <div class="compare-tomorrow-subtext">Learning Something New Every Week</div>
+
         </div>
     </div>
   </div> 

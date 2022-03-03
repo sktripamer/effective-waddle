545a45d5f57c7cb6c4fa0d27fdd3e540a5c8e1b3
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-03-03 03:27:21 -0800

    Update say-hello-list.tsx

--- src/components/say-hello-list.tsx
+++ src/components/say-hello-list.tsx
@@ -8,7 +8,7 @@ export default function sayHelloList() {
         <div class="hello-list-number-cont">
         <div class="hello-list-number">1</div>
         </div>
-        <div class="hello-list-heading">Lorum Ipsum  <div class="hello-list-subheading">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div></div>
+        <div class="hello-list-heading">BECOME AN ENTREPRENEURIAL <div class="hello-list-subheading">Get help starting a side hustle! Discover how to escape the 9-5 rat race by building a creative, meaningful, and profitable business. Maximize your job, spin off side hustles, and start a business! Entrepreneurials are anyone able to create opportunities and generate revenue for themselves as a lifestyle! Get started today with PK!</div></div>
       </div>
        
     </div>
@@ -17,7 +17,7 @@ export default function sayHelloList() {
         <div class="hello-list-number-cont">
         <div class="hello-list-number">2</div>
         </div>
-        <div class="hello-list-heading">Lorum Ipsum  <div class="hello-list-subheading">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div></div>
+        <div class="hello-list-heading">TURN YOUR IDEAS INTO INCOME  <div class="hello-list-subheading">Don’t sit on great business ideas. Don’t die in the workplace doing everything you care nothing about. Find out how to put your talents to work. Invest in yourself, not the stock market! Apply your skill sets to generate more revenue. Don’t start your side hustle from scratch. Don’t do business alone. Leverage our turn-key solutions to solve problems in real time every day!</div></div>
       </div>
       
     </div>
@@ -26,7 +26,7 @@ export default function sayHelloList() {
         <div class="hello-list-number-cont">
         <div class="hello-list-number">3</div>
       </div>
-        <div class="hello-list-heading">Lorum Ipsum  <div class="hello-list-subheading">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div></div>
+        <div class="hello-list-heading">CREATE YOUR MAXIMUM WAGE  <div class="hello-list-subheading">Don’t get minimized as an employee or executive. Get maximized as an entrepreneurial! Make the most amount of money in the least amount of time and with least amount of effort. Discover how to maximize each revenue stream. Get started with PK and learn how to acquire new skill sets to serve clients, employers, and customer!</div></div>
       </div>
       
     </div>

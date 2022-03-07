bbec68931083655178b1d0e7cb0c74c120ae6728
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-03-07 15:30:43 -0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1895,7 +1895,7 @@
                 </div>
                 </div>
                 <div class='preorder-cont-section'>
-                <h3 className="mb-222">PREORDER YOUR COPY TODAY</h3>
+                <div className="mb-222">PREORDER YOUR COPY TODAY</div>
                 <h4 className="mb-2">Your <strong>Revival of Revenue</strong> Begins Now</h4>
 
               <div className='pay-section'>

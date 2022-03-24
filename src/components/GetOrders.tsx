4a79aee8dd119a713a20d4cf946e203842c5efdd
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-03-24 02:54:53 -0700

    Update GetOrders.tsx

--- src/components/GetOrders.tsx
+++ src/components/GetOrders.tsx
@@ -5,34 +5,51 @@ import useAuth, { User } from "../hooks/useAuth";
 
 
 const GET_ORDERS = gql`
-query customer( $input: Int) {
-    customer( customerId:$input ) {
-    orders {
-      edges {
-        node {
-          id
+query theorders( $input: Int) {
+  orders(where: {customerId:$input}) {
+    edges {
+      node {
+        total
+        date
+        customerNote
+        lineItems {
+          nodes {
+            product {
+              name
+            }
+            subtotal
+          }
         }
+        transactionId
+        databaseId
       }
     }
-    }
+  }
 }
 `;
+
+
+
+//ADD ORDER TO VIA REST API SERVERLESS FUNCTION WHEN THIS HAPPENS. USE CUSTOMERNOTE FOR STRIPE DATA
+//USE REST API TO PASS PAYMENT METHOD INFOS.
+//https://stripe.com/docs/api/payment_methods/retrieve
+//https://stripe.com/docs/api/payment_methods/update
+//USE REST API TO UPDATE PAYMENT INFOS.
+
 
 export default function GetOrders() {
   const { user } = useAuth();
   const { id } = user as User;
+  let orderOutput: String;
   const { loading, error, data } = useQuery(GET_ORDERS, {
     variables: { input: parseInt(atob(id).split(':')[1]) },
   });
 
   if (loading) return <p>Loading ...</p>;
-  if (!loading) {
-    console.log(data)
-  }
+
   return (
-      
     <div>
-  Hello {data}
+  Total number of orders: aa
   { console.log(data) }
     </div>
   );

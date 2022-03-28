7b9a32adc1cc9ffa398041717951056008a4f351
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-03-28 01:17:27 -0700

    Update UnAuthContent.tsx

--- src/components/UnAuthContent.tsx
+++ src/components/UnAuthContent.tsx
@@ -5,16 +5,18 @@ import { navigate } from "gatsby";
 import useAuth from "../hooks/useAuth";
 
 export default function UnAuthContent({ children }: { children: ReactNode }) {
-  const { loggedIn, loading } = useAuth();
+  const { loggedIn, loading, user } = useAuth();
 
   // Navigate authenticated users to Members page.
   useEffect(() => {
     if (!loading && loggedIn) {
+      localStorage.setItem("auth", JSON.stringify(user.jwtAuthToken));
       navigate('/members');
     }
   }, [loggedIn, loading, navigate]);
 
   if (!loggedIn) {
+    localStorage.removeItem("auth")
     return <>{children}</>;
   }
 

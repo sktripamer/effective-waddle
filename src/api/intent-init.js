9ba4f625edc7ffd7218aa4c0b892a0bf260a5a6f
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-03-28 06:07:02 -0700

    Update intent-init.js

--- src/api/intent-init.js
+++ src/api/intent-init.js
@@ -96,8 +96,13 @@ const getCustomer = async (uEmail, uID) => {
     const responser = await axios.get('https://portal.revrevdev.xyz/wp-json/wp/v2/users/' + uID, axiosConfig)
     .then(resp => {  
        if (resp.data.acf.payment_method === '') {
-        const customerID = await createCustomer(uEmail); //create customer if none found in acf data
-        return customerID;
+           try {
+                const customerID = await createCustomer(uEmail); //create customer if none found in acf data
+                return customerID;
+           } catch(error2) {
+                return error2;
+           }
+      
        } else {
         return resp.data.acf.payment_method;
        }

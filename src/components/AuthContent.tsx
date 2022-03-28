c320c64537f01cdc990c9b60574c3a94ccd1c8f4
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-03-28 02:03:58 -0700

    Update AuthContent.tsx

--- src/components/AuthContent.tsx
+++ src/components/AuthContent.tsx
@@ -5,16 +5,25 @@ import { navigate } from "gatsby";
 import useAuth from "../hooks/useAuth";
 
 export default function AuthContent({ children }: { children: ReactNode }) {
-  const { loggedIn, loading } = useAuth();
-
+  const { loggedIn, loading, user } = useAuth();
+  const isBrowser = typeof window !== "undefined";
   // Navigate unauthenticated users to Log In page.
   useEffect(() => {
     if (!loading && !loggedIn) {
+      if (isBrowser) {
+        localStorage.removeItem("auth")
+      }
       navigate('/log-in');
     }
   }, [loggedIn, loading, navigate]);
 
   if (loggedIn) {
+    if (isBrowser) {
+      const authData = {
+        authToken: user.jwtAuthToken
+      };
+      localStorage.setItem("auth", JSON.stringify(authData));
+    }
     return <>{children}</>;
   }
 

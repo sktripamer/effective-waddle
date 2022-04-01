1804a91c9c6a0be77607bc0a556e9d7d64229246
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-03-31 19:46:30 -0700

    Update intent-verify.js

--- src/api/intent-verify.js
+++ src/api/intent-verify.js
@@ -2,12 +2,18 @@ import axios from 'axios'
 const stripe = require("stripe")(process.env.STRIPE_SECRET)
 import jwt from 'jsonwebtoken'
 let params;
-
+let paymentIntent;
 const verifyIntent = async (req, res) => {
     params = JSON.parse(req.body)
 
+    //if new account is null and so is authtoken, return error and end immediately
+    if (params.newAccount === null && params.token === null) {return res.status(400)};
+
+    //if new account is not null, and authtoken is not null, return error (and try to clearn authtoken?).
+    if (params.newAccount !== null && params.token !== null) {return res.status(400)};
+
     if (params.intent == null) return res.status(400); //no payment intent included in check, exit.
-    const paymentIntent = await stripe.paymentIntents.retrieve(params.intent);
+    paymentIntent = await stripe.paymentIntents.retrieve(params.intent);
 
     if (JSON.stringify(params.cart) !== paymentIntent.metadata.cart) return res.status(400); // cart from intent and verifier mismatch, exit.
     let total = 0;
@@ -23,6 +29,27 @@ const verifyIntent = async (req, res) => {
     const totalCartPrice = await mapLoop();
     if (totalCartPrice != paymentIntent.amount) return res.status(400); //intent and cart checker price mismatch.
     if (paymentIntent.status !== "succeeded") return res.status(400); //not successfull transaction
+
+    //if new account is not null, and authtoken is null, try to process new account.
+    if (params.newAccount !== null && params.token === null) {
+        const newUser = await createNewUser(params.newAccount);
+        return res.status(200).json({newUser})
+        // still need to create woo order
+    }
+
+    //if new account is null, and authtoken is not null, try to process authtoken.
+    if (params.newAccount === null && params.token !== null) {
+        try {
+            jwt.verify(params.token, process.env.JWT_SECRET,{ ignoreExpiration: true}, async function(err, decoded) {
+                 const savedUser = await saveUser(decoded.data.user.id); //saves acf data
+                 return res.status(200).json({savedUser})
+                // still need to create woo order
+              });
+            } catch (e) {
+                return res.status(401); //jwt not valid
+              }
+    }
+
     return res.status(200).json(true)
 }
 
@@ -49,6 +76,74 @@ const getPrice = async (pID) => {
           return err;
          })
       return responser;
+}
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+  charactersLength));
+   }
+   return result;
+  }
+
+  const createNewUser = async (newEmail) => {
+
+    let data = {
+        'username': newEmail.replace(/[^a-zA-Z0-9]/g, ''),
+        'email': newEmail,
+        'password': makeid(48),
+        "acf": {
+            "customer_id": paymentIntent.customer,
+              }
+      }
+      if (params.metafield !== null) {
+        data.acf[params.metafield] = params.metavalue
+      }
+
+      let axiosConfig = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Basic ` + process.env.REST_SECRET,
+        }
+      };
+      
+      axios.post('https://portal.revrevdev.xyz/wp-json/wp/v2/users', JSON.stringify(data), axiosConfig)
+      .then((res) => {
+       return res;
+      })
+      .catch((err) => {
+       return err;
+      })
+}
+
+const saveUser = async (userID) => {
+    let data = {
+        "acf": {
+            "customer_id": paymentIntent.customer,
+              }
+      }
+      if (params.metafield !== null) {
+        data.acf[params.metafield] = params.metavalue
+      }
+
+      let axiosConfig = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Basic ` + process.env.REST_SECRET,
+        }
+      };
+      
+      axios.post('https://portal.revrevdev.xyz/wp-json/wp/v2/users' + userID, JSON.stringify(data), axiosConfig)
+      .then((res) => {
+       return res;
+      })
+      .catch((err) => {
+       return err;
+      })
+
 }
 
 export default verifyIntent;

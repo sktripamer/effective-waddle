e9c8471e7caed8635f6c9fe06eec9b63e9793346
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-04-01 03:43:00 -0700

    Update get-payment.js

--- src/api/get-payment.js
+++ src/api/get-payment.js
@@ -7,8 +7,8 @@ const validateJWT = async (req, res) => {
     try {
             jwt.verify(req.body, process.env.JWT_SECRET,{ ignoreExpiration: true}, async function(err, decoded) {
             const customerID = await getCustomerID(decoded.data.user.id);
-            return res.status(200).json({customerID})
-            const paymentMethod = await getPaymentMethods(customerID);
+          
+            const paymentMethod = await getPaymentMethods(customerID.customerID);
 
             return res.status(200).json({paymentMethod})
          

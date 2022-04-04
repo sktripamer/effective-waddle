e397d01204cd70715dfa5e4fec4c6668209c4d57
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-04-04 05:47:09 -0700

    Update get-subscriptions.js

--- src/api/get-subscriptions.js
+++ src/api/get-subscriptions.js
@@ -1,59 +1,64 @@
-import jwt from 'jsonwebtoken'
-import axios from 'axios'
-const stripe = require("stripe")(process.env.STRIPE_SECRET);
+import * as React from "react";
+import {useState, useEffect } from "react";
 
-const validateJWT = async (req, res) => {
-//validates JWT and gets user ID from payload data. then passes to the next function to get customerID and paymentID from stripe.
-    try {
-            jwt.verify(req.body, process.env.JWT_SECRET,{ ignoreExpiration: true}, async function(err, decoded) {
-            const customerID = await getCustomerID(decoded.data.user.id);
-            if (customerID == '') return res.status(200);
-            const paymentMethod = await getPaymentMethods(customerID);
-            
-            let sublist = {};
-            for (let i=0; i< paymentMethod.data.length; i++) {
-              const paymentM = await getPaymentMethod(paymentMethod.data[i].default_payment_method)
-              sublist[i] = {
-                sub: paymentMethod.data[i],
-                pm: paymentM
-            }
-            }
-            return res.status(200).json({sublist})
-         
-       });
-      } catch (e) {
-        res.json({body: 'error ' + e})
-      } 
-}
+import useAuth, { User } from "../hooks/useAuth";
 
 
-const getCustomerID = async (uID) => {
-    let axiosConfig = {
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Basic ` + process.env.REST_SECRET,
+export default function GetSubscriptions() {
+  const { user } = useAuth();
+  const { jwtAuthToken } = user as User;
+  const [methodProcessing, setMethodProcessing] = useState(true);
+  const [arrayTest, setArray] = useState({});
+  useEffect(() => {
+    async function fetchMyAPI() {
+      try {
+        const request = await fetch('/api/get-subscriptions', {
+          method: 'POST',
+          body: jwtAuthToken,
+        });
+        const intent = (await request.json());
+        console.log(intent)
+        setArray(intent.sublist);
+        if (intent =='') {
+          setMethodProcessing(false)
+          return '';
+        } else {
+
+          setMethodProcessing(false)
+
+        return intent;
         }
-      };
-     const responser = await axios.get('https://portal.revrevdev.xyz/wp-json/wp/v2/users/' + uID, axiosConfig).then(resp => {    
-       return resp.data.acf.customer_id;
-    });
-    return responser;
+      } catch (error) {
+        console.log('Failed to get cID');
+        console.log(error);
+        return null;
+      }
+    
+    }
+    fetchMyAPI()
+  }, []);
+  const getButtonId = (e) => {
+    console.log(e.target.dataset.id)
+  }
+  return (
+    <div className='sub-list'>
+
+       {methodProcessing ? (
+            <div>loading subscriptions...</div>
+          ) : (
+            <div class='subscription-list'>
+           {arrayTest && arrayTest.map((el, index) =>
+                  <>
+                  <div data-id={el.sub.id} onClick={getButtonId} className={'sub-item'}>
+                  {/* <div className="next-payment-date">{new Date((el.sub.billing_cycle_anchor * 1000)).toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' })}</div> */}
+                  <div className='card-info'>{el.pm.id}</div>
+    </div>
+                  </>
+)}
+            </div>
+            )}
+   
+    </div>
+  );
+
 }
-
-
-  async function getPaymentMethods(cID) {
-      const paymentMethods = await stripe.subscriptions.list({
-        customer: cID
-      });
-      return paymentMethods;
-  }
-
-  async function getPaymentMethod(pm) {
-      const paymentMethod =  await stripe.paymentMethods.retrieve(
-        pm
-      );
-      return paymentMethod;
-  
-  }
-
-export default validateJWT

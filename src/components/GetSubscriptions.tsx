394df2530a7fdb2354e644c5932e2419f1ff046b
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-04-04 06:02:17 -0700

    Update GetSubscriptions.tsx

--- src/components/GetSubscriptions.tsx
+++ src/components/GetSubscriptions.tsx
@@ -47,12 +47,12 @@ export default function GetSubscriptions() {
             <div>loading subscriptions...</div>
           ) : (
             <div class='subscription-list'>
-           {arrayTest && arrayTest.map((el, index) =>
+           {arrayTest && arrayTest.map((el) =>
                   <>
-                  <div data-id={el.sub.id} onClick={getButtonId} className={'sub-item'}>
-                  {/* <div className="next-payment-date">{new Date((el.sub.billing_cycle_anchor * 1000)).toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' })}</div> */}
+                  {/* <div data-id={el.sub.id} onClick={getButtonId} className={'sub-item'}>
+                  <div className="next-payment-date">{new Date((el.sub.billing_cycle_anchor * 1000)).toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' })}</div>
                   <div className='card-info'>{el.pm.id}</div>
-    </div>
+    </div> */}
                   </>
 )}
             </div>

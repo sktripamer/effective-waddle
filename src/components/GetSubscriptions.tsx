268a4fb2c92c7136ac4ffa80a6287751b1781f97
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-04-04 06:09:29 -0700

    Update GetSubscriptions.tsx

--- src/components/GetSubscriptions.tsx
+++ src/components/GetSubscriptions.tsx
@@ -19,15 +19,7 @@ export default function GetSubscriptions() {
         const intent = (await request.json());
         console.log(intent)
         setArray(intent.sublist);
-        if (intent =='') {
           setMethodProcessing(false)
-          return '';
-        } else {
-
-          setMethodProcessing(false)
-
-        return intent;
-        }
       } catch (error) {
         console.log('Failed to get cID');
         console.log(error);

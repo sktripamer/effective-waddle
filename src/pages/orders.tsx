664f0ba2c5747d9a7874c9f34617963d541dbf8d
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-04-09 01:49:46 -0700

    Update orders.tsx

--- src/pages/orders.tsx
+++ src/pages/orders.tsx
@@ -3,10 +3,11 @@ import * as React from "react";
 import AuthContent from "../components/AuthContent";
 import Layout from "../components/Layout";
 import GetOrders from "../components/GetOrders";
-
+import Nav from "../components/Nav";
 export default function Profile() {
   return (
     <Layout>
+      <Nav />
       <h1>Orders</h1>
       <AuthContent>
         <GetOrders />

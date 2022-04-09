72df833bd9120bf161bcec670be06a0a05768930
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-04-09 04:36:19 -0700

    Update subscriptions.tsx

--- src/pages/subscriptions.tsx
+++ src/pages/subscriptions.tsx
@@ -3,14 +3,17 @@ import * as React from "react";
 import AuthContent from "../components/AuthContent";
 import Layout from "../components/Layout";
 import GetSubscriptions from "../components/GetSubscriptions";
-
+import Nav from "../components/Nav";
 export default function Profile() {
   return (
     <Layout>
+      <Nav />
+      <div className='navcontent'>
       <h1>Subscriptions</h1>
       <AuthContent>
         <GetSubscriptions />
       </AuthContent>
+      </div>
     </Layout>
   );
 }

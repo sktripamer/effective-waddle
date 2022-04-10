a348117053f81bcfd5ed4bef679865b1b07d0b82
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-04-09 23:08:37 -0700

    Update Dash.tsx

--- src/pages/Dash.tsx
+++ src/pages/Dash.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Layout from "../components/Layout";
 import Nav from "../components/Nav";
 import GetOrders from "../components/GetOrders";
@@ -11,6 +11,16 @@ import GetOrders from "../components/GetOrders";
 // markup
 const DashPage = () => {
   const [page, setPage] = useState('');
+
+  useEffect(() => {
+    if (window.location.href.indexOf("orders") > -1) {
+        setPage('orders')
+    }
+    if (window.location.href.indexOf("subscriptions") > -1) {
+      setPage('subscriptions')
+    }
+  }, []);
+
   return (
     <Layout>
       <Nav changePage={page => setPage(page)}/>

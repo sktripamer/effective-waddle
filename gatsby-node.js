76fa4b2e8726ecb995f7080fba27d1098297bfd6
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-04-15 00:17:06 -0700

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,53 +15,6 @@
 		}
 	};
 
-	// exports.createSchemaCustomization = ({ actions }) => {
-	// 	const { createTypes } = actions
-	// 	const typeDefs = `
-	// 	type products implements Node @dontInfer {	
-	// 		edges: Edges
-	// 	}
-	// 	type Edges implements Node @dontInfer {	
-	// 		node: Node
-	// 	}
-	// 	type Node implements Node @dontInfer {	
-	// 		databaseId: ID!
-	// 	}
-	// 	`
-	// 	createTypes(typeDefs)
-	// }
-
-
-
-
-	// const GET_POSTS = `
-	// query GET_POSTS {
-	// 	products {
-	// 		edges {
-	// 			node {
-	// 				databaseId
-	// 			}
-	// 		}
-	// 	}
-	// }
-	// `;
-
-	
-
-	// exports.createPages = async function({actions, graphql}) {
-	// 	const { data } = await graphql( GET_POSTS )
-
-
-	// 	data.products.edges.forEach(edge => {
-	// 		const slug = edge.node.databaseId
-	// 		const id = edge.node.databaseId
-	// 		actions.createPages({
-	// 			path: slug,
-	// 			component: require.resolve(`.src/templates/singleProduct.js`),
-	// 			context: { id },
-	// 		})
-	// 	})
-
 	exports.createSchemaCustomization = ({ actions }) => {
 		const { createTypes } = actions
 	  
@@ -77,33 +30,62 @@
 		  }
 		`)
 	  }
-
-
-
-	exports.createPages = ({ graphql, actions }) => {
-		const { createPage } = actions;
-		return new Promise((resolve, reject) => {
-		  graphql(`
-			{
-				products {
-					edges {
-						node {
-							databaseId
-						}
-					}
+	const GET_POSTS = `
+	query GET_POSTS {
+		products {
+			edges {
+				node {
+					databaseId
 				}
 			}
-		  `).then(result => {
-			result.data.products.edges.forEach(({ node }) => {
-			  createPage({
-				path: node.databaseId,
-				component: path.resolve(`.src/templates/singleProduct.js`),
-				context: {
-				 id: node.databaseId,
-				},
-			  });
-			});
-			resolve();
-		  });
-		});
-	  };
+		}
+	}
+	`;
+
+	
+
+	exports.createPages = async function({actions, graphql}) {
+		const { data } = await graphql( GET_POSTS )
+
+
+		data.products.edges.forEach(edge => {
+			const slug = edge.node.databaseId
+			const id = edge.node.databaseId
+			actions.createPages({
+				path: slug,
+				component: require.resolve(`.src/templates/singleProduct.js`),
+				context: { id },
+			})
+		})
+	}
+
+
+
+
+	// exports.createPages = ({ graphql, actions }) => {
+	// 	const { createPage } = actions;
+	// 	return new Promise((resolve, reject) => {
+	// 	  graphql(`
+	// 		{
+	// 			products {
+	// 				edges {
+	// 					node {
+	// 						databaseId
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	  `).then(result => {
+	// 		result.data.products.edges.forEach(({ node }) => {
+	// 		  createPage({
+	// 			path: node.databaseId,
+	// 			component: path.resolve(`.src/templates/singleProduct.js`),
+	// 			context: {
+	// 			 id: node.databaseId,
+	// 			},
+	// 		  });
+	// 		});
+	// 		resolve();
+	// 	  });
+	// 	});
+	//   };

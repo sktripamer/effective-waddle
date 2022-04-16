5a168b4ffee2585efc9c9941cd8fb3bd0e8b5eba
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-04-15 20:45:50 -0700

    Update singleProduct.js

--- src/templates/singleProduct.js
+++ src/templates/singleProduct.js
@@ -5,23 +5,31 @@ const singleProduct = ( props ) => {
 
     const { pageContext: { id, slug } } = props;
 
+    const query = gql`
+    query SingleProductQuery($id: String!) {
+        product(id: $id, idType: DATABASE_ID) {
+            description
+            name
+          }
+    }
+    `
+    
+      const { loading, error, data } = useQuery(query, {
+        variables: { input: id },
+      });
+    
+      if (loading) return <p>Loading ...</p>;
+
+
 console.log(props.pageContext)
     return (
         <div>
             aa
-          <div>{id}</div>
-          <div>{slug}</div>
+          <div>{data.product.name}</div>
+          <div>{data.product.description}</div>
         </div>
     );
 }
 
 export default singleProduct;
 
-export const query = gql`
-query SingleProductQuery($id: String!) {
-    product(id: $id, idType: DATABASE_ID) {
-        description
-        name
-      }
-}
-`

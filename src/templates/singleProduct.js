b89b11799555d7950996935d6623ad320f2a628c
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-04-16 18:36:59 -0700

    Update singleProduct.js

--- src/templates/singleProduct.js
+++ src/templates/singleProduct.js
@@ -3,16 +3,49 @@ import { useQuery, gql } from "@apollo/client";
 
 const singleProduct = ( props ) => {
 
-    const { pageContext: { id, slug } } = props;
+    const { pageContext: { id, slug, name, description, cat, type } } = props;
+let query;
 
-    const query = gql`
+if (type === "SIMPLE") {
+    query = gql`
     query SingleProductQuery($id: ID!) {
-        product(id: $id) {
-            description
-            name
+        product(id: $id, idType: DATABASE_ID) {
+            ... on SimpleProduct {
+                price
+              }
           }
     }
     `
+} else {
+    query = gql`
+    query SingleProductQuery($id: ID!) {
+        product(id: $id, idType: DATABASE_ID) {
+            ... on VariableProduct {
+                variations {
+                  nodes {
+                    databaseId
+                    attributes {
+                      nodes {
+                        value
+                      }
+                    }
+                    name
+                  }
+                }
+                attributes {
+                  nodes {
+                    name
+                    options
+                  }
+                }
+                price
+              }
+          }
+    }
+    `
+}
+
+    
     
       const { loading, error, data } = useQuery(query, {
         variables: { id: id },
@@ -24,10 +57,27 @@ const singleProduct = ( props ) => {
 console.log(props.pageContext)
     return (
         <div>
+        {type === "SIMPLE"
+        ? (
+            
+          
+            <div>
             aa
           <div>{data.product.name}</div>
           <div>{data.product.description}</div>
         </div>
+        
+          )
+        : 
+        <div>
+       yy
+      <div>{data.product.name}</div>
+      <div>{cool.data.product.attributes.nodes[0].name}</div>
+    </div>
+        
+        }
+    
+    </div>
     );
 }
 

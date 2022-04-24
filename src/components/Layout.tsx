03e914dbddf6880fd4ed5b9eab31379efe0cf565
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-04-23 19:45:00 -0700

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,11 +3,11 @@ import { ReactNode } from "react";
 import { Helmet } from 'react-helmet';
 import Header from "./Header";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }, { htmlClassName = '' }: {htmlClassName: string}) {
   return (
     <>
           <Helmet>
-        <html lang="en" />
+        <html className={htmlClassName} lang="en" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>

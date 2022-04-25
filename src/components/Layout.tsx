5a3d9fc7a15f521ce0a741cd82a5845eae36c47d
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-04-25 05:40:21 -0700

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,10 +9,11 @@ interface IProps {
 
 
 export default function Layout({ children }: { children: ReactNode },{ htmlClassName }: IProps) {
+  console.log(htmlClassName)
   return (
     <>
           <Helmet >
-        <html className='test' lang="en" />
+        <html className={htmlClassName} lang="en" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>

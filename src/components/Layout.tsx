9cc0cc6c5dee1c982151579a040b245182ef225d
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-04-25 07:59:49 -0700

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,136 +1,31 @@
-import React from 'react';
-import { useQuery, gql } from "@apollo/client";
-import { useState , useEffect } from "react";
-import Layout from "../components/Layout";
-import Navbar from "../components/Navbar";
-const singleProduct = ( props ) => {
-    const [varSelect, varSelector] = useState();
-    const [clickedItem, setClickedItem] = useState(0);
-    const [count, setCount] = useState(1);
-    const { pageContext: { id, slug, name, description, cat, type } } = props;
-let query;
+import * as React from "react";
+import { ReactNode } from "react";
+import { Helmet } from 'react-helmet';
+import Header from "./Header";
 
-if (type === "SIMPLE") {
-    query = gql`
-    query SingleProductQuery($id: ID!) {
-        product(id: $id, idType: DATABASE_ID) {
-            ... on SimpleProduct {
-                price
-              }
-          }
-    }
-    `
-} else {
-    query = gql`
-    query SingleProductQuery($id: ID!) {
-        product(id: $id, idType: DATABASE_ID) {
-            ... on VariableProduct {
-                variations {
-                  nodes {
-                    databaseId
-                    attributes {
-                      nodes {
-                        value
-                      }
-                    }
-                    name
-                  }
-                }
-                attributes {
-                  nodes {
-                    name
-                    options
-                  }
-                }
-                price
-              }
-          }
-    }
-    `
-}
-const VariationCart = (e) => {
-    console.log(count)
-    if (varSelect===undefined) {
-        console.log(data.product.variations.nodes[0].databaseId)
-    } else {
-        console.log(varSelect)
-    } 
-   
-}
-const variationClick = (e) => {
-    console.log(e.target.dataset.id)
-    //find variation and set it
-    setClickedItem(parseInt(e.target.dataset.idindex));
-    varSelector(data.product.variations.nodes[e.target.dataset.idindex].databaseId)
-  }
-
-      const { loading, error, data } = useQuery(query, {
-        variables: { id: id },
-      });
-
-      if (loading) return <p>Loading ...</p>;
-    
-
-console.log(props.pageContext)
-
-let incrementCount = () => {
-    setCount(count + 1);
-  };
-
-  let decrementCount = () => {
-      if (count >= 2) {
-        setCount(count - 1);
-      }
-
-  };
-
-  const changeCount = (e) => {
-      setCount(parseInt(e.target.value.replace(/\D/,'')))
-  }
-
-    return (
-        <Layout htmlClassName={"scroll"}>
-        <Navbar />
-        <div>
-        {type === "SIMPLE"
-        ? (
-            
-          
-            <div>
-            aa
-          <div>{data.product.name}</div>
-          <div>{data.product.description}</div>
-        </div>
-        
-          )
-        : 
-        <div>
-       yy
-      <div>{data.product.name}</div>
-      <div>{data.product.attributes.nodes[0].name}</div>
-      {data.product.attributes.nodes[0].options && data.product.attributes.nodes[0].options.map((el, index) =><div className={index === clickedItem ? "is-checked" : ""} onClick={variationClick} data-idindex={index} data-id={el}>{el}</div>)}
-      <div>
-        <div class="count">
-      
-     <input type="tel" value={count} onChange={changeCount}/>
-          <h3>Count:</h3>
-          <h1>{count}</h1>
-        </div>
-        <div class="buttons">
-          <button onClick={decrementCount}>-</button>
-          <button onClick={incrementCount}>+</button>
-        </div>
-      </div>
-      
-      <button onClick={VariationCart}>add to cart</button>
-    </div>
-        
-        }
-    
-    </div>
-    </Layout>
-    );
+interface IProps {
+  htmlClassName: string
+  children: ReactNode
 }
 
-export default singleProduct;
 
+export default function Layout({ htmlClassName, children }: IProps) {
+  console.log(htmlClassName)
+  return (
+    <>
+          <Helmet>
+        <html className={htmlClassName} lang="en" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+<link rel="manifest" href="/site.webmanifest"/>
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+<meta name="msapplication-TileColor" content="#da532c"/>
+<meta name="theme-color" content="#ffffff"/>
+      </Helmet>
+      <title>revrevdev</title>
+
+      <main>{children}</main>
+    </>
+  );
+}

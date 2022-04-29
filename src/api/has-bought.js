847eff25df8f0f953802cd78502238b95e557c97
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-04-29 06:11:24 -0700

    Create has-bought.js

---
+++ src/api/has-bought.js
@@ -0,0 +1,29 @@
+
+import jwt from 'jsonwebtoken'
+import axios from 'axios'
+
+
+let params;
+const validateJWT = async (req, res) => {
+    params = JSON.parse(req.body)
+    //validates JWT and gets user ID from payload data. then passes to the next function to get customerID and paymentID from stripe.
+        try {
+                jwt.verify(params.token, process.env.JWT_SECRET,{ ignoreExpiration: true}, async function(err, decoded) {
+                const customerID = await addCourse(decoded.data.user.id, params.product);
+                return res.status(200).json({customerID})
+             
+           });
+          } catch (e) {
+            res.json({body: 'error ' + e})
+          } 
+    }
+
+const addCourse = async(customerID, productID) => {
+    const exists = await axios.get('https://portal.revrevdev.xyz/?better_ld_api=d74dd1094863071982578684bc13be64&better_ld_api_method=has_bought&course_id=' + productID + '&user_id=' + customerID).then(resp => {    
+      return resp.data;
+    });
+    return exists;
+
+}
+
+export default validateJWT

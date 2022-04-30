797e7de22426335a1a403b7b034c397bdd96dd0b
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-04-30 01:53:21 -0700

    Update singleProduct.js

--- src/templates/singleProduct.js
+++ src/templates/singleProduct.js
@@ -558,16 +558,16 @@ const renderDelivery = () => {
 
     <div className='product-data-tabs'>
         <div className='product-tabs-cont'>
-         {/* <div onClick={changeTab(0)} className='product-tabs-desc'>Description</div>
+         <div onClick={changeTab(0)} className='product-tabs-desc'>Description</div>
          <div onClick={changeTab(1)} className='product-tabs-advanced'>Additional Information</div>
-         <div onClick={changeTab(2)} className='product-tabs-reviews'>Reviews ({data.product.reviewCount})</div>
+         {/* <div onClick={changeTab(2)} className='product-tabs-reviews'>Reviews ({data.product.reviewCount})</div>
          <div onClick={changeTab(3)} className='product-tabs-delivery'>Delivery and Returns</div> */}
         </div>
         <div className='render-tab'>
             <>
         {tab === 0 && renderDescripton()}
-        {/* {tab === 1 && renderAddtional()}
-        {tab === 2 && renderWriteReview()}
+        {tab === 1 && renderAddtional()}
+        {/* {tab === 2 && renderWriteReview()}
         {tab === 3 && renderDelivery()} */}
             </>
         </div>

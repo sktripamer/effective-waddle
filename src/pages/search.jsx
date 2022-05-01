04c66f583ca4c0c1d5a2f0edc1e8d000b953e1cf
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-05-01 09:47:08 -0700

    Update search.jsx

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,9 +1,25 @@
 import * as React from "react";
 import SearchSystem from '../components/SearchSystem';
-import pageQuery from '../components/SearchSystem';
 
 export default function SearchPage() {
   return (
     <SearchSystem data={pageQuery}/>
   );
 }
+
+export const pageQuery = graphql`
+query {
+    products {
+      nodes {
+        slug
+        name
+        productCategories {
+          nodes {
+            slug
+          }
+        }
+      }
+    }
+  }
+  
+`

b46b1aa10cfb8b47d98283d4a450b6935b75471e
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-05-01 09:04:11 -0700

    Create search.js

---
+++ src/components/search.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function SearchBar({ searchQuery, setSearchQuery }) {
+return (
+    <form
+        action="/"
+        method="get"
+        autoComplete="off"
+    >
+        <label htmlFor="header-search">
+            <span className="visually-hidden">
+                Search blog posts
+            </span>
+        </label>
+        <input
+            value={searchQuery}
+            onInput={(e) => setSearchQuery(e.target.value)}
+            type="text"
+            id="header-search"
+            placeholder="Search blog posts"
+            name="s"
+        />
+        <button type="submit">Search</button>
+    </form>
+)
+};

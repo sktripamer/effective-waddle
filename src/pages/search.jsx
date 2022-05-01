4dea4fb265cb2b3ad45f1eb0bdb046e06b71aa70
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-05-01 09:49:44 -0700

    Update search.jsx

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import SearchSystem from '../components/SearchSystem';
-
+import { graphql } from 'gatsby';
 export default function SearchPage() {
   return (
     <SearchSystem data={pageQuery}/>

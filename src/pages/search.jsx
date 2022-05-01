6d20014986f0ebd40e81de40b17722cd3b767cb9
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-05-01 09:59:14 -0700

    Update search.jsx

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,11 +1,32 @@
 import * as React from "react";
-import SearchSystem from '../components/SearchSystem';
 import { graphql } from 'gatsby';
-export default function SearchPage() {
-  return (
-    <SearchSystem data={pageQuery}/>
+import SearchBar from '../components/search';
+
+export default ({
+  data: {
+      products: { nodes },
+  },
+}) => {
+  const { search } = window.location;
+  const query = new URLSearchParams(search).get('s')
+  const [searchQuery, setSearchQuery] = useState(query || '');
+
+  const posts = nodes;
+
+ return (
+      <div>
+          <h1>Blog</h1>
+          <SearchBar
+              searchQuery={searchQuery}
+              setSearchQuery={setSearchQuery}
+          />
+          {posts.map(post => (
+               <div>{post.name}</div>
+          ))}
+      </div>
   );
-}
+};
+
 
 export const pageQuery = graphql`
 query {

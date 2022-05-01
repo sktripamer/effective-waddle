b0a6e899c9471b140f59cf1a08c3f5cc2b1d0c53
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-05-01 07:31:16 -0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -154,40 +154,24 @@ export default function Navbar(props) {
   return (
     <>
      {loading ? (
-     <navbar class="active"><h4>Revival of Revenue</h4></navbar>
+     <navbar class="active"><h4>Menu</h4></navbar>
      ) : (
       <navbar class="active">
         {sideNav()}
-      <h4>Revival of Revenue</h4>
+      <h4>Menu</h4>
       <ul>
-        
-        <li>Home</li>
-          <a class='dropdown'>Shop
-      <div class="link-dropdown active2">
-      <label>Shirt Collections</label>
-      <label>Hat Collections</label>
-      <label>All Products</label>
-    </div></a>
-          <a class='dropdown'>Resources
-      <div class="link-dropdown active2">
-      <label>Everything Entrepreneurial Blog</label>
-      <label>Help Desk</label>
-      <label>Contact</label>
-    </div></a>
+        <li>Shop</li>
     <a class='dropdown'>Cart
       <div class="link-dropdown active2 cart-drop">
       <div>{localStorageSetHandler()}</div>
     </div></a>
         {!loggedIn ? (
           <>
-              <li>Log in</li>
           </>
         ) : (
           <>
-           <li>My Account</li>
           </>
         )}
-        <li>Preorder my book!</li>
       </ul>
     </navbar>
      )}

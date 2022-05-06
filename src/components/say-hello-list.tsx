38495d38521db345a07d2b974c6be8be33940257
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-05-06 14:18:43 -0700

    Update say-hello-list.tsx

--- src/components/say-hello-list.tsx
+++ src/components/say-hello-list.tsx
@@ -9,8 +9,9 @@ export default function sayHelloList() {
         <div class="hello-list-number">1</div>
         </div>
         <div class='hello-list-content-cont'>
-        <div class="hello-list-heading">BECOME AN ENTREPRENEURIAL</div>
-        <div class="hello-list-subheading">Get help starting a side hustle! Discover how to escape the 9-5 rat race by building a creative, meaningful, and profitable business. Maximize your job, spin off side hustles, and start a business! Entrepreneurials are anyone able to create opportunities and generate revenue for themselves as a lifestyle! Get started today with PK!</div>
+        <div class="hello-list-heading">YOU ARE A BUSINESS</div>
+        <div class="white-sh-var">Build Your Business</div>
+        <div class="hello-list-subheading">Discover how to escape the 9-5 rat race, get help starting a side hustle, and build your own creative, meaningful, and profitable business. Invest in yourself, not just the stock market! Entrepreneurials are anyone able to create multiple businesses and generate multiple revenue streams as a lifestyle! Anyone can succeed by following the right system!</div>
         </div>
       </div>
        
@@ -21,7 +22,9 @@ export default function sayHelloList() {
         <div class="hello-list-number">2</div>
         </div>
         <div class='hello-list-content-cont'>
-        <div class="hello-list-heading">TURN YOUR IDEAS INTO INCOME</div>  <div class="hello-list-subheading">Don’t sit on great business ideas. Don’t die in the workplace doing everything you care nothing about. Find out how to put your talents to work. Invest in yourself, not the stock market! Apply your skill sets to generate more revenue. Don’t start your side hustle from scratch. Don’t do business alone. Leverage our turn-key solutions to solve problems in real time every day!</div></div>
+        <div class="hello-list-heading">YOU ARE A MINISTRY</div>
+        <div class="white-sh-var">Lead Your Ministry</div>
+        <div class="hello-list-subheading">Who doesn’t want  to go to work and build something  you believe in? You spend the majority of your life at work trying to make money. Doesn’t it make sense to make God a part of that pursuit? After all, the dollar bill still bears God’s Name. Many of us WANT more of God in life but NEED to make more money. PK is Putting God Back into the Equation of Making Money. What if your work and your life mission can finally work together? Build a business you believe in!</div></div>
       </div>
       
     </div>
@@ -31,7 +34,9 @@ export default function sayHelloList() {
         <div class="hello-list-number">3</div>
       </div>
       <div class='hello-list-content-cont'>
-        <div class="hello-list-heading">CREATE YOUR MAXIMUM WAGE </div> <div class="hello-list-subheading">Don’t get minimized as an employee or executive. Get maximized as an entrepreneurial! Make the most amount of money in the least amount of time and with least amount of effort. Discover how to maximize each revenue stream. Get started with PK and learn how to acquire new skill sets to serve clients, employers, and customer!</div></div>
+        <div class="hello-list-heading">YOU ARE A VOTE</div>
+        <div class="white-sh-var">Stand for Freedom</div>
+        <div class="hello-list-subheading">Don’t simply vote in elections and expect change in the world. We need entrepreneurial leaders who solve real world problems. Successfully execute your business plan and life goals despite the obstacles. During these times of uncertain global economics and unclear politics, learn how to make a difference in this world while also generating profitability in your world. Discover how to navigate the gauntlet of challenges that entrepreneurial leaders face today as you start your personal entrepreneurial journey.</div></div>
       </div>
       
     </div>

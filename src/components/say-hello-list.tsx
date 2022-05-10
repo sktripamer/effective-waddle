652fdf05fd11ce6d0f5c8149643d00ac6ac36e3d
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-05-10 05:40:43 -0700

    Update say-hello-list.tsx

--- src/components/say-hello-list.tsx
+++ src/components/say-hello-list.tsx
@@ -9,7 +9,7 @@ export default function sayHelloList() {
         <div class="hello-list-number">1</div>
         </div>
         <div class='hello-list-content-cont'>
-        <div class="hello-list-heading">YOU ARE A BUSINESS</div>
+        <div class="hello-list-heading">YOUR ROADMAP TO REVENUE</div>
         <div class="white-sh-var">Build Your Business</div>
         <div class="hello-list-subheading">Discover how to escape the 9-5 rat race, get help starting a side hustle, and build your own creative, meaningful, and profitable business. Invest in yourself, not just the stock market! Entrepreneurials are anyone able to create multiple businesses and generate multiple revenue streams as a lifestyle! Anyone can succeed by following the right system!</div>
         </div>
@@ -22,7 +22,7 @@ export default function sayHelloList() {
         <div class="hello-list-number">2</div>
         </div>
         <div class='hello-list-content-cont'>
-        <div class="hello-list-heading">YOU ARE A MINISTRY</div>
+        <div class="hello-list-heading">YOUR PATHWAY TO PROFITABILITY</div>
         <div class="white-sh-var">Lead Your Ministry</div>
         <div class="hello-list-subheading">Who doesn’t want  to go to work and build something  you believe in? You spend the majority of your life at work trying to make money. Doesn’t it make sense to make God a part of that pursuit? After all, the dollar bill still bears God’s Name. Many of us WANT more of God in life but NEED to make more money. PK is Putting God Back into the Equation of Making Money. What if your work and your life mission can finally work together? Build a business you believe in!</div></div>
       </div>
@@ -34,7 +34,7 @@ export default function sayHelloList() {
         <div class="hello-list-number">3</div>
       </div>
       <div class='hello-list-content-cont'>
-        <div class="hello-list-heading">YOU ARE A VOTE</div>
+        <div class="hello-list-heading">YOUR COMPASS FOR COMPANY CREATORS</div>
         <div class="white-sh-var">Stand for Freedom</div>
         <div class="hello-list-subheading">Don’t simply vote in elections and expect change in the world. We need entrepreneurial leaders who solve real world problems. Successfully execute your business plan and life goals despite the obstacles. During these times of uncertain global economics and unclear politics, learn how to make a difference in this world while also generating profitability in your world. Discover how to navigate the gauntlet of challenges that entrepreneurial leaders face today as you start your personal entrepreneurial journey.</div></div>
       </div>

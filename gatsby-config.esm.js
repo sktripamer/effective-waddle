9ac0d1868c637557bbdf0d2e0fd1d8098e13ff84
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-05-13 05:03:45 -0700

    Update gatsby-config.esm.js

--- gatsby-config.esm.js
+++ gatsby-config.esm.js
@@ -62,7 +62,7 @@ module.exports = {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
-          include: /assets/,
+          include: /\.inline\.svg$/,
         },
       },
     },

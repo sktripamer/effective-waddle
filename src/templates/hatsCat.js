ea60a7dd3ece72da0eca66e5cc92a97629e5c679
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-05-18 06:07:45 -0700

    Update hatsCat.js

--- src/templates/hatsCat.js
+++ src/templates/hatsCat.js
@@ -12,11 +12,73 @@ import WriteReview from "../components/WriteReview";
 const hatsCat = ( props ) => {
     const { pageContext: { pagedata } } = props;
 
-console.log(pagedata)
 
-return (
-    <div>{JSON.stringify(pagedata)}</div>
-)
+    const heroRenderer = (item) => {
+        return (
+            <div className='image-gallery-image'>
+              <img
+                  src={item.original}
+              />
+      
+              {
+                item.title &&
+                  <span className='image-gallery-description'>
+                    {item.title}
+                    {
+                      item.link &&
+                        <a className='my-own-class' href={item.link}>
+                          {item.title}
+                        </a>
+                    }
+                  </span>
+              }
+              <span className='image-gallery-price'>
+                {item.price}
+              </span>
+            </div>
+          )
+
+
+    }
+    let items = [];
+
+    useEffect(() => {
+        pagedata.data.products.edges.forEach( edge => {
+            if (edge.node.featured === true) {
+                let tempObj = {
+                    "original": edge.node.featuredImage.node.sourceUrl,
+                    "thumbnail": edge.node.featuredImage.node.sourceUrl,
+                    "title": edge.node.name,
+                    "link": edge.node.slug,
+                    "price": edge.node.price,
+                    "renderItem": heroRenderer
+                }
+                items.push(tempObj)
+            }
+        })
+    }, []);
+
+
+
+    console.log(pagedata)
+
+    return (
+        <Layout>
+        <Navbar />
+        <div>
+        <h1>Revival of Revenue Shirts Collection</h1>
+        <ImageGallery items={items}/>
+
+
+
+        </div>
+
+
+
+        {/* <div>{JSON.stringify(pagedata)}</div> */}
+
+        </Layout>
+    )
 
 
 }

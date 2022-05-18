ff17c2544c4ab13e6512590f4440679b07b0f3c8
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-05-18 11:34:43 -0700

    Update ProductLister.jsx

--- src/components/ProductLister.jsx
+++ src/components/ProductLister.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StarRating from '../components/StarRating';
+import {navigate} from 'gatsby';
 export default function ProductLister(props) {
     let renderItems = []
     props.allData.data.products.edges.forEach( edge => {
@@ -15,18 +16,20 @@ export default function ProductLister(props) {
 
     
     })
-
+    const changePage = (e) => {
+        navigate(`shirts/${e.target.dataset.link}`)
+    }
 
 return (
     <>
-    {renderItems.map(item => <Link to={`shirts/${item.node.slug}`}>
-              <div class='mini-product-cont'>
+    {renderItems.map(item =>
+              <div onClick={changePage} data-link={item.node.slug} class='mini-product-cont'>
         <img src={item.node.featuredImage.node.sourceUrl}/>
         <div class='mini-title'>{item.node.name}</div>
         <div class='mini-review'>{<StarRating total={item.node.averageRating}/>}</div>
         <div class='mini-price'>{item.node.price}</div>
     </div>
-    </Link>)}    
+   )}    
     
     </>
 

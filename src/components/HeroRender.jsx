6698937552628583d5f08ea65b9945d0a173c7e9
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-05-28 20:51:10 -0700

    Update HeroRender.jsx

--- src/components/HeroRender.jsx
+++ src/components/HeroRender.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { useState, useEffect, useRef } from "react";
 import ImageGallery from 'react-image-gallery';
+import { useSpringCarousel } from 'react-spring-carousel'
 
 export default function HeroRender(props) {
+
+    const goToProduct = (e) => {
+        console.log(e.target.dataset.idlink)
+        navigate(e.target.dataset.idlink)
+    }
 
     const heroRenderer = (item) => {
         return (
@@ -35,6 +41,7 @@ export default function HeroRender(props) {
 
     useEffect(() => {
         let items = [];
+        let itemIndex = 1;
         props.herodata.data.products.edges.forEach( edge => {
             if (edge.node.featured === true) {
                 let tempObj = {
@@ -43,19 +50,40 @@ export default function HeroRender(props) {
                     "title": edge.node.name,
                     "link": 'shirts/' + edge.node.slug,
                     "price": edge.node.price,
-                    "renderItem": heroRenderer
+                    "id": `item-${itemIndex}`
                 }
                 items.push(tempObj)
+                itemIndex++;
             }
         })
         setUseItems(items)
 
     }, []);
     console.log(useItems)
+    const { 
+        carouselFragment, 
+        slideToPrevItem, 
+        slideToNextItem 
+      } = useSpringCarousel({
+        withLoop: true,
+        items: useItems.map((i) => ({
+            id: i.id,
+            renderItem: (
+              <div onClick={goToProduct} idindex={i.link} className='heroproduct-render'>
+                <div className='heroproduct-img-cont'><img src={i.original}/></div>
+                <div className='heroproduct-title'>{i.title}</div>
+                <div className='heroproduct-price'>{i.price}</div>
+              </div>
+            ),
+          })),
+      });
 return (
     <>
-    {console.log(useItems)}
-    <ImageGallery showThumbnails={false} showFullscreenButton={false} showPlayButton={false} items={useItems}/>
+        <div>
+        <button className='heroprev' onClick={slideToPrevItem}>Prev item</button>
+        {carouselFragment}
+        <button className='heronext' onClick={slideToNextItem}>Next item</button>
+      </div>
     </>
 )
 

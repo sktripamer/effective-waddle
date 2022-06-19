5051a5456865075465a773d87b05be0ab41ba0fa
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-06-18 21:26:14 -0700

    Update useOnScreen.jsx

--- src/hooks/useOnScreen.jsx
+++ src/hooks/useOnScreen.jsx
@@ -2,11 +2,17 @@ import { useEffect, useState } from "react"
 
 export default function useOnScreen(ref, rootMargin = "0px") {
   const [isVisible, setIsVisible] = useState(false)
-
+  const [firstVisible, setFirstVisible] = useState(false)
+  if (firstVisible === true) return true;
   useEffect(() => {
     if (ref.current == null) return
     const observer = new IntersectionObserver(
-      ([entry]) => setIsVisible(entry.isIntersecting),
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting)
+        if (entry.isIntersecting) {
+            setFirstVisible(true)
+        }
+      },
       { rootMargin }
     )
     observer.observe(ref.current)

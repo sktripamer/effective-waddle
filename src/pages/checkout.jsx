8c2ac03b2e96a9728672daa0a8862e336375b770
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-06-20 02:01:48 -0700

    Update checkout.jsx

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -15,7 +15,7 @@ async function handleSubmit(e) {
         code:couponCode
     }
     const request = await fetch('/api/check-coupon', {
-        method: 'POST',
+        method: 'GET',
         body: JSON.stringify(ex),
       });
       const intent = (await request.json());

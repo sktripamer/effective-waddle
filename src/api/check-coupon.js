6fded07bfae9bff55843c7492247024a7702176c
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-06-20 03:44:02 -0700

    Update check-coupon.js

--- src/api/check-coupon.js
+++ src/api/check-coupon.js
@@ -10,44 +10,34 @@ const checkCoupon = async (req, res) => {
    // return res.status(200).json(safeCode)
     if (safeCode == '') return res.status(400)
     console.log('here')
-   // const couponInfo = await getCoupon(safeCode)
-   // console.log(couponInfo)
-   let aaa;
-!async function(){
-let data = await fetch('https://portal.revrevdev.xyz/wp-json/wc/v3/coupons/?code=' + safeCode, {
-    headers: {
-        'Authorization':  `Basic ` + process.env.WC_SECRET
-    }
-})
-    .then((response) => {
-        console.log('hererr')
-        console.log(response.text())
-        return response.text()})
-    .catch(error => {
-        console.error(error);
-    });
-
-aaa = JSON.parse(data)
-}();
-  await res.status(200).json({aaa})
+   const couponInfo = await getCoupon(safeCode)
+   console.log(couponInfo)
+   return res.status(200).json({couponInfo})
 }
 
-const getCoupon = async (code) => {
-    let axiosConfig = {
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Basic ` + process.env.WC_SECRET,
-        }
-    };
-      const responser = await axios.get('https://portal.revrevdev.xyz/wp-json/wc/v3/coupons/?code=' + code, axiosConfig)
-      .then(resp => {  
-          console.log(resp)
-         return resp.text()
-      })
-      .catch((err) => {
-          return err;
-         })
-      return responser;
-}
+// const getCoupon = async (code) => {
+//     let axiosConfig = {
+//         headers: {
+//             'Content-Type': 'application/json',
+//             Authorization: `Basic ` + process.env.WC_SECRET,
+//         }
+//     };
+//       const responser = await axios.get('https://portal.revrevdev.xyz/wp-json/wc/v3/coupons/?code=' + code, axiosConfig)
+//       .then(resp => {  
+//           console.log(resp)
+//          return resp.text()
+//       })
+//       .catch((err) => {
+//           return err;
+//          })
+//       return responser;
+// }
+const getCoupon = async(code) => {
+    console.log('hererr')
+    const exists = await axios.get('https://portal.revrevdev.xyz/wp-json/wc/v3/coupons/?code=' + code, {headers: { Authorization: `Basic ` + process.env.WC_SECRET}}).then(resp => {    
+      return resp.data;
+  });
+  return exists;
 
+  }
 export default checkCoupon;

e450db8390ef3b87cb8c878d63a310aa57cdfe24
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-06-20 02:34:17 -0700

    Update check-coupon.js

--- src/api/check-coupon.js
+++ src/api/check-coupon.js
@@ -3,8 +3,9 @@ let params;
 const checkCoupon = async (req, res) => {
     params = JSON.parse(req.body);
     //params.code, params.cart
-    return  res.status(200).json(params.code)
+   // return  res.status(200).json(params.code)
     const safeCode = params.code.replace(/[^a-zA-Z0-9]/g, "");
+    return res.status(200).json(safeCode)
     if (safeCode == '') return res.status(400)
     const couponInfo = await getCoupon(safeCode)
     return res.status(200).json({couponInfo})

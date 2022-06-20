7b316781a10cda89039f19c7668f117d0ee388fa
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-06-20 01:29:27 -0700

    Update check-coupon.js

--- src/api/check-coupon.js
+++ src/api/check-coupon.js
@@ -3,7 +3,7 @@ let params;
 const checkCoupon = async (req, res) => {
     params = JSON.parse(req.body);
     //params.code, params.cart
-    safeCode = params.code.replaceAll("[^a-zA-Z0-9]", "");
+    const safeCode = params.code.replaceAll("[^a-zA-Z0-9]", "");
     if (safeCode == '') return res.status(400)
     const couponInfo = await getCoupon(safeCode)
     return res.status(200).json({couponInfo})
@@ -16,7 +16,7 @@ const getCoupon = async (code) => {
             Authorization: `Basic ` + process.env.WC_SECRET,
         }
     };
-      const responser = await axios.get('https://portal.revrevdev.xyz/wp-json/wc/v3/coupons/?code=' +code, axiosConfig)
+      const responser = await axios.get('https://portal.revrevdev.xyz/wp-json/wc/v3/coupons/?code=' + code, axiosConfig)
       .then(resp => {  
         return resp;
       })

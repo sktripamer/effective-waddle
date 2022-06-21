e81e84ec713a6ce95590d3123adc09ec393f9d06
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-06-20 22:08:47 -0700

    Update check-coupon.js

--- src/api/check-coupon.js
+++ src/api/check-coupon.js
@@ -11,8 +11,29 @@ const checkCoupon = async (req, res) => {
     if (safeCode == '') return res.status(400)
     console.log('here')
    const couponInfo = await getCoupon(safeCode)
-   console.log(couponInfo)
-   return res.status(200).json({couponInfo})
+   if (couponInfo.length === 1) {
+    if (couponInfo[0].discount_type === 'fixed_product' && couponInfo[0].limit_usage_to_x_items === 1) {
+        //fixed product discount and only applies once
+        cartIndex = -1;
+        couponInfo[0].product_ids.forEach((couponProduct, index) => {
+            params.cart.forEach((cartProduct, index2) => {
+              if (couponProduct === cartProduct.ID) cartIndex = index2;
+            })
+        })
+        if (cartIndex >= 0) {
+            //coupon is valid and in the users cart
+            let newprice = params.cart[cartIndex].price - parseFloat(coupon.couponInfo[0].amount);
+            params.cart[cartIndex].price = newprice;
+            params.cart[cartIndex].code = couponInfo[0].code;
+            let newCart =  params.cart
+            return res.status(200).json({success: true, newCart})
+        }
+        
+    }
+    //more cases here
+    return res.status(200).json({success: false}) //coupon is valid, but no matching cases
+   }
+   return res.status(200).json({success: false}) //coupon not valid
 }
 
 // const getCoupon = async (code) => {

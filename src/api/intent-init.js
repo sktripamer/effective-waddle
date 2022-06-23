bfcdf77a1e442fc50618b96213e16425d4618b82
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-06-22 18:50:24 -0700

    Update intent-init.js

--- src/api/intent-init.js
+++ src/api/intent-init.js
@@ -23,17 +23,40 @@ const getIntent = async (req, res) => {
         if (!exists) {
             const customerID = await createCustomer(params.newAccount);
 
+            let couponInvalid = false;
             let total = 0;
             const mapLoop = async _ => {
                 const promises = params.cart.map(async pID => {
                     const priceOfProduct = await getPrice(pID)
-                    total += (parseInt(priceOfProduct) * pID.quantity)
+                    if (pID.ctype === 'fp1') {
+                       const safeCode = pID.code.replace(/[^a-zA-Z0-9]/g, "");
+                       const couponInfo = await getCoupon(safeCode)
+                       if (couponInfo.length === 1) {
+                           if (couponInfo[0].discount_type === 'fixed_product' && couponInfo[0].limit_usage_to_x_items === 1) {
+                               let productMatch = false;
+                               couponInfo[0].product_ids.forEach((couponProduct, index) => {
+                                   if (couponProduct == pID.ID) productMatch = true;
+                               })
+                               if (productMatch === true) {
+                                   let productprice = parseInt(priceOfProduct);
+                                   total += (productprice * pID.quantity) + (productprice - parseFloat(couponInfo[0].amount))
+
+                               }
+                           }
+                         couponInvalid = true;
+                       }
+                       couponInvalid = true;
+                    } else {
+                       total += (parseInt(priceOfProduct) * pID.quantity)
+                    }
+                    
                     return priceOfProduct
                 })
                 const totalPrice = await Promise.all(promises)
                 return total;
             }
             const totalCartPrice = await mapLoop();
+            if (couponInvalid === true) return res.status(400).json({success: false}) 
            const paymentIntent = await createIntent(customerID, totalCartPrice);
            return res.status(200).json({paymentIntent})
         } else {
@@ -47,18 +70,40 @@ const getIntent = async (req, res) => {
         try {
         jwt.verify(params.token, process.env.JWT_SECRET,{ ignoreExpiration: true}, async function(err, decoded) {
              const customerID = await getCustomer(decoded.data.user.id); // tries to retrieve the ACF data (stripe customer ID) associated with the decoded JWT
-
+             let couponInvalid = false;
              let total = 0;
              const mapLoop = async _ => {
                  const promises = params.cart.map(async pID => {
                      const priceOfProduct = await getPrice(pID)
-                     total += (parseInt(priceOfProduct) * pID.quantity)
+                     if (pID.ctype === 'fp1') {
+                        const safeCode = pID.code.replace(/[^a-zA-Z0-9]/g, "");
+                        const couponInfo = await getCoupon(safeCode)
+                        if (couponInfo.length === 1) {
+                            if (couponInfo[0].discount_type === 'fixed_product' && couponInfo[0].limit_usage_to_x_items === 1) {
+                                let productMatch = false;
+                                couponInfo[0].product_ids.forEach((couponProduct, index) => {
+                                    if (couponProduct == pID.ID) productMatch = true;
+                                })
+                                if (productMatch === true) {
+                                    let productprice = parseInt(priceOfProduct);
+                                    total += (productprice * pID.quantity) + (productprice - parseFloat(couponInfo[0].amount))
+
+                                }
+                            }
+                          couponInvalid = true;
+                        }
+                        couponInvalid = true;
+                     } else {
+                        total += (parseInt(priceOfProduct) * pID.quantity)
+                     }
+                     
                      return priceOfProduct
                  })
                  const totalPrice = await Promise.all(promises)
                  return total;
              }
              const totalCartPrice = await mapLoop();
+             if (couponInvalid === true) return res.status(400).json({success: false}) 
              if (customerID === '') { //no saved customer ID on stripe, so we create a new one and pass off that intent.
                 const newCustomerID = await createCustomer(params.newAccount);
                 const paymentIntent = await createIntent(newCustomerID, totalCartPrice);
@@ -114,12 +159,25 @@ const getCustomer = async (uID) => {
 const createIntent = async (cID, cartTotal) => {
     let metacart = []
     params.cart.forEach((item, index) => {
-    let pusher = {
-            i: item.ID,
-            q: item.quantity,
-            p: item.price,
-            t: item.total
-    }
+        let pusher;
+        if (item.ctype === 'fp1') {
+            pusher = {
+                i: item.ID,
+                q: item.quantity,
+                p: item.price,
+                t: item.total,
+                c: item.code,
+                cq: item.cquantity,
+                cp: item.cprice
+            }
+        } else {
+            pusher = {
+                i: item.ID,
+                q: item.quantity,
+                p: item.price,
+                t: item.total
+            }
+        }  
     metacart.push(pusher)
 })
     try {
@@ -145,7 +203,8 @@ const getPrice = async (pID) => {
         }
     };
       const responser = await axios.get('https://portal.revrevdev.xyz/wp-json/wc/v3/products/' + pID.ID, axiosConfig)
-      .then(resp => {  
+      .then(resp => {
+
           //if cart contains coupon for this ID, double check the coupon is valid as such, and reduce price
         if (resp.data.price.includes('.')) {
             return resp.price.replace(/\./g, '');
@@ -159,6 +218,12 @@ const getPrice = async (pID) => {
       return responser;
 }
 
+const getCoupon = async(code) => {
+    const exists = await axios.get('https://portal.revrevdev.xyz/wp-json/wc/v3/coupons/?code=' + code, {headers: { Authorization: `Basic ` + process.env.WC_SECRET}}).then(resp => {    
+      return resp.data;
+  });
+  return exists;
 
+  }
 
 export default getIntent;

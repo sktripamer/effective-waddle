6bf7ec544f59725eb7fc22c94831640afd07bada
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-06-25 23:52:31 -0700

    Update log-in.tsx

--- src/pages/log-in.tsx
+++ src/pages/log-in.tsx
@@ -7,7 +7,6 @@ import LogInForm from "../components/LogInForm";
 export default function LogIn() {
   return (
     <Layout>
-      <h1>Log In</h1>
       <UnAuthContent>
         <LogInForm />
       </UnAuthContent>

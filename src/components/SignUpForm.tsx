2fffbb394e5d160ad191e6760e69bf27f7a9d9a0
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-06-28 04:48:56 -0700

    Update SignUpForm.tsx

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,85 +1,124 @@
 import * as React from "react";
-import { useMutation, gql } from "@apollo/client";
-import { Link } from "gatsby";
+import { Link, navigate } from "gatsby";
+import { useState } from "react";
 
-const REGISTER_USER = gql`
-  mutation registerUser(
-    $email: String!
-    $password: String!
-  ) {
-    registerUser(
-      input: {
-        username: $email
-        email: $email
-        password: $password
-      }
-    ) {
-      user {
-        databaseId
-      }
+function validateEmail(email) 
+    {
+        var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(email);
     }
-  }
-`;
 
-export default function SignUpForm() {
-  const [register, { data, loading, error }] = useMutation(REGISTER_USER);
-  const wasSignUpSuccessful = Boolean(data?.registerUser?.user?.databaseId);
 
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const values = Object.fromEntries(data);
-    register({
-      variables: values,
-    }).catch(error => {
-      console.error(error);
-    });
-  }
+export default function LogInForm() {
 
-  if (wasSignUpSuccessful) {
-    return (
-      <p>
-        Thanks! Check your email – an account confirmation link has been sent to you.
-      </p>
-    )
+  const [loginStep, setLoginStep] = useState(0);
+  const [loadingUser, setLoadingUser] = useState(false);
+  const [emailValidError, setEmailValidError] = useState(false)
+  const [goToEmail, setGoToEmail] = useState('')
+  const [emailText, setEmailText] = useState('')
+  
+
+async function handleVerify() {
+  setEmailValidError(false)
+  let emailvalue = ((document.getElementById('log-in-email') as HTMLInputElement)).value
+  setEmailText(emailvalue)
+  setLoadingUser(true)
+  if (validateEmail(emailvalue) === false) {
+    setLoadingUser(false)
+    setEmailValidError(true)
+    return;
   }
 
+  const request = await fetch('/api/verify-account', {
+    method: 'POST',
+    body: emailvalue,
+  });
+  const intent = (await request.json());
+  setLoadingUser(false)
+  console.log(intent)
+  if (intent.exists.message === true) {
+    //returns true if the account exists and has notregistered value, or if email doesnt exist.
+    let splitEmail = emailvalue.split('@');
+    setGoToEmail(splitEmail[splitEmail.length - 1])
+    //trigger email
+ 
+    setLoginStep(2)
+  } else {
+    setEmailValidError(true)
+    setLoginStep(0)
+  }
+}
+
+  
   return (
-    <form method="post" onSubmit={handleSubmit}>
-      <fieldset disabled={loading} aria-busy={loading}>
-        
-        <label htmlFor="sign-up-email">Email</label>
+  <div className={`login-page loginstep-${loginStep} checkuser-${loadingUser}`}>
+        <div className={`verify-sent-container`}>
+    {2 === loginStep
+                ? (
+                 <div className='search-outer-cont'>
+                   <div className="search-bar-title">
+                      <div className="search-bar-text">Verify Email</div>
+                      <div onClick={() =>  setLoginStep(0)} className='search-bar-close'>X</div>
+                   </div>
+                   <div class='email-sent'>
+                     <div class="verify-email-text">{`Please click the link that we sent to ${emailText} to confirm your account.`}</div>
+                   <div id="anim-wrapper">
+  <div id="anim-bg">
+    <div id="env-wrapper">
+      <div class="speedline line1"></div>
+      <div class="speedline line2"></div>
+      <div class="speedline line3"></div>
+    <i id="env" class="aasd"></i>
+
+    </div>
+  </div>
+  
+    <div id="check-container">
+    <div class="check-stroke1"></div>
+    <div class="check-stroke2"></div>
+  </div>
+
+</div>
+<a class='goto-email' href={`https://${goToEmail}`} target="_blank" rel="noreferrer noopener">
+  {`Go To ${goToEmail.substr(0, goToEmail.lastIndexOf("."))}`}
+</a>
+                     </div>
+                   </div>
+                  )
+                : ""}
+    </div>
+     <h1 onClick={() => navigate('/')}>Revival of Revenue</h1>             
+    <form className={`login-form`} method="post">
+      <fieldset >
+      <h2>Sign Up</h2>      
+        <div className="login-email">
+        <div class="inputwrap emailer">
         <input
-          id="sign-up-email"
-          type="email"
+          id="log-in-email"
+          type="text"
           name="email"
-          autoComplete="username"
+          autoComplete="email"
           required
         />
-         <label htmlFor="sign-up-password">Password</label>
-        <input
-          id="sign-up-password"
-          type="password"
-          name="password"
-          autoComplete="password"
-          required
-        />
-        {error ? (
-          (error.message.includes('This username is already registered') ? (
-            <p className="error-message">
-              You&#39;re already signed up! <Link to="/log-in">Log in</Link>
-            </p>
-          ) : (
-            <p className="error-message">{error.message}</p>
-          ))
-        ) : null}
-        <button type="submit" disabled={loading}>
-          {loading ? 'Signing up...' : 'Sign up'}
-        </button>
+        <div class="label">Email</div>
+        </div>
+        {emailValidError === true ? (
+          <p className="error-message">Invalid email. Please try again again or use a different email.</p>
+        ) : null}      
+        <button className="verify-user-button" onClick={handleVerify} disabled={loadingUser}>Next</button>
+        </div>
+        
+
+
       </fieldset>
-      <p>
-        Already have an account? <Link to="/log-in"><a>Log in</a></Link>
+      <p className="account-sign-up-message">
+       Already have an account?{' '}
+        <Link to="/log-in">
+          Log In
+        </Link>
       </p>
     </form>
+    
+  </div>
   );
 }

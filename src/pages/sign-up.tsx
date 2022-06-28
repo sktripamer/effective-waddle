f9c912d7dccda3df7ed5f7588918603619d4f261
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-06-28 04:43:15 -0700

    Update sign-up.tsx

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -7,7 +7,6 @@ import SignUpForm from "../components/SignUpForm";
 export default function SignUp() {
   return (
     <Layout>
-      <h1>Sign Up</h1>
       <UnAuthContent>
         <SignUpForm />
       </UnAuthContent>

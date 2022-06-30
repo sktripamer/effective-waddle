15dddc9b0fa4d6b43ac3152f24c82df596749522
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-06-29 23:42:05 -0700

    Update GetSubscriptions.tsx

--- src/components/GetSubscriptions.tsx
+++ src/components/GetSubscriptions.tsx
@@ -9,6 +9,8 @@ export default function GetSubscriptions() {
   const { jwtAuthToken } = user as User;
   const [methodProcessing, setMethodProcessing] = useState(1);
   const [arrayTest, setArray] = useState({});
+  const [loadSearch, setLoadSearch] = useState(false)
+  const [loadingPaymentData, setLoadingPaymentData] = useState(true)
   useEffect(() => {
     async function fetchMyAPI() {
       try {
@@ -38,30 +40,71 @@ export default function GetSubscriptions() {
     }
   });
   console.log(arrayTest)
-  const getButtonId = (e) => {
+  const getButtonId = async (e) => {
+    setLoadingPaymentData(true)
+    setLoadSearch(true)
     console.log(e.target.dataset.id)
+    const request = await fetch('/api/get-subscriptions', {
+      method: 'POST',
+      body: e.target.dataset.paymentid,
+    });
+    const intent = (await request.json());
+    console.log(intent)
+    setLoadingPaymentData(false)
   }
   return (
     <div className='sub-list'>
 
        {methodProcessing === 1 ? (
+         <>
+           <h2>Subscriptions</h2>
+           <h3>View and manage your subscriptions</h3>
             <div class='subloading'>Loading subscriptions...</div>
+            </>
           ) : ('')}
            {methodProcessing === 2 ? (
-            <div class='subloading'>No active subscriptions!</div>
+         <>
+         <h2>Subscriptions</h2>
+         <h3>View and manage your subscriptions</h3>
+          <div class='subloading'>No subscriptions found!</div>
+          </>
           ) : ('')}
           
           {methodProcessing === 0 ? (
+            <>
+                <div className={`search-btn-container search-${loadSearch}`}>
+              {true == loadSearch
+                ? (
+                 <div className='search-outer-cont'>
+                   <div className="search-bar-title">
+                      <div className="search-bar-text">Subscription</div>
+                      <div onClick={() => setLoadSearch(false)} className='search-bar-close'>X</div>
+                   </div>
+                    {loadingPaymentData === true ? (
+                      <div class='loading-bought'></div>
+                    )
+                    : (
+                     'loaded'
+                    )}
+                   </div>
+                  )
+                : ""}
+    </div>
+            <h2>Subscriptions</h2>
+            <h3>View and manage your subscriptions</h3>
             <div class='subscription-list'>
                {arrayTest && arrayTest.map((el, index) =>
                   <>
-                  <div data-id={el.id} onClick={getButtonId} className={'sub-item'}>
-                  <div className="next-payment-date">{new Date((el.billing_cycle_anchor * 1000)).toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' })}</div>
-                  <div className='card-info'>{el.default_payment_method}</div>
-    </div>
+                  <div data-paymentid={el.default_payment_method} data-id={el.id} onClick={getButtonId} className={'sub-item'}>
+                    <div className="next-payment-date">{new Date((el.billing_cycle_anchor * 1000)).toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' })}</div>
+                    <div className='sub-name'>{el.plan.id === 'price_1LFzPWEIi9OXKxaBADloi95c' ? 'Entrepreneurial Espresso' : ''}</div>
+                    <div className='planactive'>{el.plan.active === true ? 'ACTIVE' : 'INACTIVE'}</div>
+                    <div className='plancost'>{`$${el.plan.amount.toString().substring(0,el.plan.amount.toString().length-2)+"."+el.plan.amount.toString().substring(el.plan.amount.toString().length-2)}`}</div>
+                  </div>
                   </>
 )}
 </div>
+</>
             ): ('')}
    
     </div>

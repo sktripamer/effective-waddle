ad5601d0b5ad31eae1772f5d28a99e08f1914193
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-06-29 19:59:15 -0700

    Update singleProduct.js

--- src/templates/singleProduct.js
+++ src/templates/singleProduct.js
@@ -905,22 +905,50 @@ const renderDelivery = () => {
       }      
  <div className='product-pricer'><h3>{data.product.price}</h3></div>
 <div className='product-short-desc'><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p></div>
-<div className='buttons-addtocart'>
-        <div class="buttons">
-          <button onClick={decrementCount}>-</button>
-          <input type="tel" value={count} onChange={changeCount}/>
-          <button onClick={incrementCount}>+</button>
-        </div>
-        {variationNoStock===true ? 
-        (
-        <div class='no-stock-text'>This item variation is out of stock! Try selecting a different one.</div>
-        ):''}
-        {addedToCart === true ? (
+{addedToCart === true ? (
          <div class='added-cart-cont'> <div class='added-to-cart-text'>Product added to cart!</div><button onClick={() => setOpenCart(true)} class='viewcart'>View Cart</button></div>
         ): (
         <></>
         )}
+<div className='buttons-addtocart'>
+          {data.product.productCategories.nodes[0].slug === 'courses' ?
+          (
+            <></>
+          )
+          :
+          (
+            <div class="buttons">
+            <button onClick={decrementCount}>-</button>
+            <input type="tel" value={count} onChange={changeCount}/>
+            <button onClick={incrementCount}>+</button>
+          </div>
+          )}
+
+
+        {variationNoStock===true ? 
+        (
+        <div class='no-stock-text'>This product is out of stock! Try selecting a different one.</div>
+        ):''}
+        
+        {data.product.productCategories.nodes[0].slug === 'courses' && hasBought === true ?
+              (<>
+        {data.product.databaseId === 1735 ? (
+              <div>subscription status is already active. manage your subscription at the account page.</div>
+        ):(
+            <div>you've already bought this course! view your courses here.</div>
+        )}
+              </>):
+              (<>
+{data.product.productCategories.nodes[0].slug === 'courses' ? (
+          <button disabled={addedToCart} className={`addcart-btn added-${addedToCart}`} onClick={SimpleCart}>{addedToCart === false ? 'Add to cart' : 'Added to cart'}</button>
+        ): (
         <button disabled={variationNoStock} className={`addcart-btn added-${addedToCart}`} onClick={SimpleCart}>{addedToCart === false ? 'Add to cart' : 'Change item'}</button>
+     
+        )}   
+              </>)}
+        
+        
+     
       </div>    
          
 
@@ -1158,6 +1186,11 @@ const renderDelivery = () => {
       ) :('')
     }
       </div>
+      {addedToCart === true ? (
+         <div class='added-cart-cont'> <div class='added-to-cart-text'>Product added to cart!</div><button onClick={() => setOpenCart(true)} class='viewcart'>View Cart</button></div>
+        ): (
+        <></>
+        )}
       <div className='buttons-addtocart'>
         <div class="buttons">
           <button onClick={decrementCount}>-</button>

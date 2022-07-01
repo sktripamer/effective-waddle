044b8d36b34f0fee6870734ec85a00520e39f9f1
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-07-01 04:07:47 -0700

    Update GetPayments.tsx

--- src/components/GetPayments.tsx
+++ src/components/GetPayments.tsx
@@ -47,7 +47,8 @@ export default function GetPayments() {
   }, []);
 
   useEffect(() => {
-    window.location.reload();
+    if (showSuccess===true)  window.location.reload();
+  
   }, [showSuccess]);
 
   useEffect(() => {

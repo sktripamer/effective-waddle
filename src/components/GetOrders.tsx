55fd25fe046a4995a62d28dec97e672084ca1054
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-07-01 01:36:44 -0700

    Update GetOrders.tsx

--- src/components/GetOrders.tsx
+++ src/components/GetOrders.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { useQuery, gql } from "@apollo/client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useAuth, { User } from "../hooks/useAuth";
 
 
@@ -45,16 +45,9 @@ query theorders( $input: Int) {
 
 `;
 
-
-
-//ADD ORDER TO VIA REST API SERVERLESS FUNCTION WHEN THIS HAPPENS. USE CUSTOMERNOTE FOR STRIPE DATA
-//USE REST API TO PASS PAYMENT METHOD INFOS.
-//https://stripe.com/docs/api/payment_methods/retrieve
-//https://stripe.com/docs/api/payment_methods/update
-//USE REST API TO UPDATE PAYMENT INFOS.
-
-
 export default function GetOrders() {
+  const [selectedOrder, setSelectedOrder] = useState(null)
+  const [loadSearch, setLoadSearch] = useState(false)
   useEffect(() => {
     console.log(window.location.href.indexOf("orders"))
     if (window.location.href.indexOf("orders") === -1) {
@@ -62,6 +55,22 @@ export default function GetOrders() {
       window.location.reload();
     }
   });
+
+
+
+  const getButtonId = async (e) => {
+    setSelectedOrder(newA[e.target.dataset.idx])
+    setLoadSearch(true)
+
+
+
+  }
+
+  const closeModal = () => {
+    setLoadSearch(false)
+    setSelectedOrder(null)
+  }
+
   const { user } = useAuth();
   const { id } = user as User;
   let orderOutput: String;
@@ -69,7 +78,14 @@ export default function GetOrders() {
     variables: { input: parseInt(atob(id).split(':')[1]) },
   });
 
-  if (loading) return <p>Loading ...</p>;
+  if (loading) return (
+    <div className='sub-list'>
+      <h2>Orders</h2>
+           <div class='sub-subheader'>View and manage your orders</div>
+            <div class='subloading'>Loading orders...</div>
+
+    </div>
+  );
 
   let newA = []
 for (let i=0; i < data.orders.edges.length; i++) {
@@ -77,8 +93,61 @@ for (let i=0; i < data.orders.edges.length; i++) {
 }
 
 
-  return (<div>
-  {newA && newA.map((el) => <h1>order # {el.node.databaseId}</h1>)}
-  { console.log(data) }
-</div>);
+  return (
+    <div className='sub-list'>
+
+<div className={`search-btn-container search-${loadSearch}`}>
+              {true == loadSearch
+                ? (
+                 <div className='search-outer-cont'>
+                   <div className="search-bar-title">
+                      <div className="search-bar-text">Subscription</div>
+                      <div onClick={closeModal} className='search-bar-close'>X</div>
+                   </div>
+                   
+                      <div class='subscription-section-modal'> 
+                      {selectedOrder === null ? (
+                          <div class='loading-bought'></div>
+                      ): (
+                        <>
+                        <div class='subscription-modal-head'>Order #{selectedOrder.node.id}</div>
+                        <div class='cart-all-items'>
+                        {selectedOrder.node.lineItems.nodes.map((el, index) =>
+                        <>
+                        <div class="cart-cont"><img class="cart-img" height="82" width="82" src={el.product.image.sourceUrl}/><div class="name-total-cart-cont"><div class="cart-name">{el.product.name}</div><div class="cart-item-total">${parseFloat(el.subtotal).toLocaleString('en-us', {minimumFractionDigits: 2})}</div></div></div>
+                        </>
+                        )}
+                        </div>
+                        </>
+                      )}
+
+                  </div>
+                   </div>
+                  )
+                : ""}
+    </div>
+
+      <h2>Orders</h2>
+       <div class='sub-subheader'>View and manage your orders</div>
+       <div class='subscription-list'>
+
+
+  {newA && newA.map((el, index) => 
+  <>
+              <div data-idx={index} onClick={getButtonId} className={'sub-item'}>
+                    <div class='planactive-subname'>
+                     <div className={`planactive plan-true`}>{el.node.status}</div>
+                     <div className='sub-name'>Order #{el.node.databaseId}</div>
+                    </div>
+                    <div className="next-payment-date">{new Date(el.node.date).toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' })}</div>
+                    <div className='plancost'>{el.node.total}</div>
+                    <div data-idx={index} className='more-sub-dets'>More Details</div>
+                  </div>
+  </>
+  )}
+
+</div>
+
+</div>
+);
 }

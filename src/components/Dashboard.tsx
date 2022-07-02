886445469bcdb1fc7076bba94f77dfaea003eb24
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-07-01 23:51:59 -0700

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,23 +3,32 @@ import { ReactNode } from "react";
 import { Helmet } from 'react-helmet';
 import Header from "./Header";
 
-export default function Dashboard({ children }: { children: ReactNode }) {
+interface IProps {
+  htmlClassName: string
+  seoImage: string
+  seoTitle: string
+  seoTitleTemplate: string
+  seoDescription: string
+  children: ReactNode
+}
+
+
+export default function Layout({ seoTitleTemplate, seoTitle, seoImage, seoDescription, htmlClassName, children }: IProps) {
   return (
     <>
-          <Helmet>
-        <html lang="en" />
+          <Helmet  title={seoTitle} titleTemplate={seoTitleTemplate}>
+        <html className={htmlClassName} lang="en" />
+        <meta name="description" content={seoDescription} />
+      <meta name="image" content={seoImage} />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
 <link rel="manifest" href="/site.webmanifest"/>
 <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
-<meta content="yes" name="apple-mobile-web-app-capable" />
-
-<meta name="viewport" content="width=device-width, initial-scale = 0.88, maximum-scale=3.0, minimum-scale=0.88"/>
 <meta name="msapplication-TileColor" content="#da532c"/>
 <meta name="theme-color" content="#ffffff"/>
       </Helmet>
-      <title>revrevdev</title>
+
       <main>{children}</main>
     </>
   );

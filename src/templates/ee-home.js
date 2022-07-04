ca4611511ae31b8d3db8db0f779911c2c0ef435c
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-07-04 02:32:42 -0700

    Create ee-home.js

---
+++ src/templates/ee-home.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import { useQuery, gql } from "@apollo/client";
+import { useState , useEffect } from "react";
+import Layout from "../components/Layout";
+import Navbar from "../components/Navbar";
+import { navigate } from 'gatsby';
+
+const hatsCat = ( props ) => {
+
+    const { pageContext: { pagedata } } = props;
+    const [loadingBought, setLoadingBought] = useState(true)
+    const [bought, setBought] = useState(false)
+
+    const email = function() {
+        try {
+          return JSON.parse(localStorage.auth).authToken;
+        } catch {
+          return null;
+        }
+      }
+
+    useEffect(() => {
+        async function fetchMyAPI() {
+          try {
+            if (email() === null) {
+              console.log('nothing')
+              return;
+            }
+           let sendBody = {
+                token: email(),
+            }
+
+            const request = await fetch('/api/check-ee', {
+              method: 'POST',
+              body: JSON.stringify(sendBody),
+            });
+            const intent = (await request.json());
+            console.log(intent)
+
+            if (intent.customerID.success === 1) {
+                setBought(true)
+            }
+
+            setLoadingBought(false)
+          } catch (error) {
+            console.log('Failed to get cID');
+            console.log(error);
+            return null;
+          }
+        
+        }
+        fetchMyAPI()
+
+      }, []);
+
+      const preReveal = () => {
+
+        navigate('/shop/courses/entrepreneurial-espresso-subscription')
+    }
+      const changePage = (slug) => {
+        let temp = '/entrepreneurial-espresso/' + slug
+        navigate(temp)
+    }
+
+      return (
+          <>
+          <Layout htmlClassName={"scroll"}>
+        <Navbar />
+            <div class='ee-home'>
+                <h2>Entrepreneurial Espresso</h2>
+                <h3>Exclusive content to fuel your daily life</h3>
+                {loadingBought === false ? (
+                    <div class='loader-white'></div>
+                ) : (
+                    <>
+
+                {bought === false ? (
+                    <>
+                    <div class='ee-optin'>Get access to this exclusive content!</div>
+                    <div onClick={preReveal} class="y-preorderbtn">Get Access</div>
+                    </>
+                ): (
+                    <>
+                    </>
+                )}
+
+                    <div class={`all-products-cont ee-${bought}`}>
+                    {pagedata.data.posts.edges.map(item =>
+                        <div onClick={changePage(item.node.slug)} class={`mini-product-cont`}>
+                            <img src={item.node.featuredImage.node.sourceUrl}/>
+                            <div class='mini-title'>{item.node.title}</div>
+                            <div class='mini-text'>{item.node.excerpt}</div>
+                        </div>
+                    )}
+                </div>   
+                </>
+                )}
+     
+
+
+            </div>
+
+
+            </Layout>
+          </>
+      )
+
+}
+
+
+
+export default hatsCat;

9250f40ce4742716327e38e554ac7b89bde5a8f1
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-07-05 06:06:13 -0700

    Update change-sub.js

--- src/api/change-sub.js
+++ src/api/change-sub.js
@@ -1,12 +1,10 @@
-import axios from 'axios'
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
-import jwt from 'jsonwebtoken';
 let params;
 
 
 const getIntent = async (req, res) => {
     params = JSON.parse(req.body);
-    const paymentIntent = await createIntent(params.sub, params.pid);
+    const paymentIntent = await createIntent(params.sub, params.pid, params.canceled);
     return res.status(200).json({paymentIntent});
 
 }
@@ -14,12 +12,13 @@ const getIntent = async (req, res) => {
 
 
 
-const createIntent = async (subID, methodID) => {
+const createIntent = async (subID, methodID, cancel) => {
 
     try {
         const subscription = await stripe.subscriptions.update(
             subID,
-            {default_payment_method: methodID}
+            {default_payment_method: methodID},
+            {cancel_at_period_end: !cancel}
           );
      
       return subscription;

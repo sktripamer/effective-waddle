4da39549f10aca45935122bd9610c276f2550d7b
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-07-04 20:48:03 -0700

    Create HeroRenderShop.jsx

---
+++ src/components/HeroRenderShop.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { navigate } from 'gatsby';
+import { useState, useEffect, useRef } from "react";
+import ImageGallery from 'react-image-gallery';
+import { useSpringCarousel } from 'react-spring-carousel-js'
+
+export default function HeroRender(props) {
+
+    const goToProduct = (e) => {
+        console.log(e)
+        console.log(e.target.dataset.idlink)
+        navigate(e.target.dataset.idlink)
+    }
+
+ const featureFilter = props.herodata.data.products.edges.filter(edge => edge.node.featured === true);
+const [windowWidth, setWindowWidth] = useState(2)
+  const [useItems, setUseItems] = useState([]);
+  const reportWindowSize = () => {
+    if (window.innerWidth > 666) {
+        setWindowWidth(2)
+    } else {
+        setWindowWidth(1)
+    }
+  }
+    useEffect(() => {
+        setWindowWidth(window.innerWidth)
+        if (window.innerWidth > 666) {
+            setWindowWidth(2)
+        } else {
+            setWindowWidth(1)
+        }
+        let items = [];
+        let itemIndex = 1;
+        props.herodata.data.products.edges.forEach( edge => {
+            if (edge.node.featured === true) {
+                let tempObj = {
+                    "original": edge.node.featuredImage.node.sourceUrl,
+                    "thumbnail": edge.node.featuredImage.node.sourceUrl,
+                    "title": edge.node.name,
+                    "link": 'shirts/' + edge.node.slug,
+                    "price": edge.node.price,
+                    "id": `item-${itemIndex}`
+                }
+                items.push(tempObj)
+                itemIndex++;
+            }
+        })
+        setUseItems(items)
+        window.addEventListener('resize', reportWindowSize);
+        return () => {
+          window.removeEventListener('resize', reportWindowSize);
+        };
+    }, []);
+
+    console.log(useItems)
+
+    const { 
+        carouselFragment, 
+        slideToPrevItem, 
+        slideToNextItem 
+      } = useSpringCarousel({
+        withLoop: true,
+        itemsPerSlide: windowWidth,
+        items: featureFilter.map( (i, index) => 
+            
+            
+            ({
+
+            id: `item-${index}`,
+            renderItem: (
+              <div onClick={goToProduct} data-idlink={`${i.node.productCategories.nodes[0].slug}/${i.node.slug}`} className='heroproduct-render'>
+
+                <div style={Object.assign({'background': `url(${i.node.featuredImage.node.sourceUrl})` }, {'background-size': 'contain'}, {'background-repeat': 'no-repeat'}, {'background-position': 'center'})} className='heroproduct-img-cont'></div>
+                <div className='heroproduct-title'>{i.node.name}</div>
+                <div className='heroproduct-price'>{i.node.price}</div>
+              </div>
+            ),
+            
+          })
+          ),
+      });
+return (
+    <>
+        <div className='heroproduct-cont'>
+        <button className='heroprev' onClick={slideToPrevItem}></button>
+        {carouselFragment}
+        <button className='heronext' onClick={slideToNextItem}></button>
+      </div>
+    </>
+)
+
+
+}

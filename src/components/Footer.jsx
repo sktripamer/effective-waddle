d8e8719b12b3634dd5716896d30ba54ab9303ec1
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-07-04 22:38:42 -0700

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+export default function Footer() {
+
+    return (
+    <footer>
+        <div class='footer-cont'>
+
+            <div class="footer-items">
+                <p class='footer-title'>Get Started</p>
+                <a href='#'>RevRev Starter Kit</a>
+                <a href='#'>The TM5 Matrix Course</a>
+                <a href='#'>The Exodus Workshop</a>
+                <a href='#'>Ask PK Consultation</a>
+                <a href='#'>Entrepreneurial Espresso Challenge</a>
+            </div>
+        
+            <div class="footer-items">
+                <p class='footer-title'>Shop</p>
+                <a href='/shop/courses'>Courses</a>
+                <a href='/shop/hats'>Hats</a>
+                <a href='/shop/shirts'>Shirts</a>
+                <a href='/shop/accessories'>Accessories</a>
+                <a href='/shop'>All Products</a>
+            </div>
+            
+            <div class="footer-items">
+                <p class='footer-title'>Enroll</p>
+                <a href='#'>RevRev Starter Kit</a>
+                <a href='#'>The TM5 Matrix Course</a>
+                <a href='#'>The Exodus Workshop</a>
+                <a href='#'>Ask PK Consultation</a>
+                <a href='#'>Entrepreneurial Espresso Challenge</a>
+            </div>
+            
+            <div class="footer-items">
+                <p class='footer-title'>Resources</p>
+                <a href='#'>RevRev Starter Kit</a>
+                <a href='#'>The TM5 Matrix Course</a>
+                <a href='#'>The Exodus Workshop</a>
+                <a href='#'>Ask PK Consultation</a>
+                <a href='#'>Entrepreneurial Espresso Challenge</a>
+            </div>
+
+
+        </div>
+
+
+
+    </footer>
+
+    )
+
+}

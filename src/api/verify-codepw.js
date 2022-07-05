c0e0019a92bb171c883406da3b3b08807a04c502
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-07-04 23:38:27 -0700

    Create verify-codepw.js

---
+++ src/api/verify-codepw.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+let params;
+const verifyUser = async (req, res) => {
+    params = JSON.parse(req.body);
+    const exists = await axios.get('https://portal.revrevdev.xyz/?better_ld_api=d74dd1094863071982578684bc13be64&better_ld_api_method=verify_codepw&useremail=' + params.e + '&code=' + params.c).then(resp => {    
+        return resp.data;
+    });
+    return res.status(200).json({exists})
+
+}
+
+export default verifyUser

edaf919cf79e7e425020221aaafa1090aa084f6b
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-07-08 03:34:28 -0700

    Update test.jsx

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,62 +1,65 @@
-import React, { useState, useRef, useEffect } from 'react';
-import AuthContent from "../components/AuthContent";
-import Layout from "../components/Layout";
-import { navigate } from 'gatsby';
-import { useQuery, gql, useMutation } from "@apollo/client";
-    import {loadStripe} from '@stripe/stripe-js/pure';
-    import useAuth, { User } from "../hooks/useAuth";
-import {
-    CardElement,
-    Elements,
-    useStripe,
-    useElements,
-  } from '@stripe/react-stripe-js'; 
+    import React, { useState, useRef, useEffect } from 'react';
+    import AuthContent from "../components/AuthContent";
+    import Layout from "../components/Layout";
+    import { navigate } from 'gatsby';
+    import { useQuery, gql, useMutation } from "@apollo/client";
+        import {loadStripe} from '@stripe/stripe-js/pure';
+        import useAuth, { User } from "../hooks/useAuth";
+    import {
+        CardElement,
+        Elements,
+        useStripe,
+        useElements,
+    } from '@stripe/react-stripe-js'; 
 
-  const CREATE_ORDER = gql` 
-  mutation createNewOrder($databaseId: Integer) {
-    createOrder(input: {customerId: $databaseId}) {
-        order {
-          databaseId
+    const CREATE_ORDER = gql` 
+    mutation createNewOrder($databaseId: Integer) {
+        createOrder(input: {customerId: $databaseId}) {
+            order {
+            databaseId
+            }
         }
     }
-  }
-`;
+    `;
 
 
-export default function Test() {
-  
-   
+    export default function Test() {
+    
+    
 
-   
+    
 
-    return (
-        <AuthContent>
-            <Cool/>
-        </AuthContent>
-    )
-}
+        return (
+            <AuthContent>
+                <Cool/>
+            </AuthContent>
+        )
+    }
 
-const Cool = () => {
-    const { user } = useAuth();
-    const { databaseId } = user;
-    const [createNewOrder, { data, loading, error }] = useMutation(CREATE_ORDER);
+    const Cool = () => {
+        const { user } = useAuth();
+        const { databaseId } = user;
+        const [createNewOrder, { data, loading, error }] = useMutation(CREATE_ORDER);
 
-    createNewOrder({
-        variables: { databaseId },
-      }).catch(error => {
-        console.error(error);
-      });
-      return (
-        <>
-        <button onClick={newOrder}>new order</button>
-        {loading === true ? <div>loading...</div> : ''}
-        {data ? (
+        const newOrder = () => {
+            updateProfile({
+                variables: { id, ...values, },
+            }).catch(error => {
+                console.error(error);
+            });
+        }
+
+        return (
             <>
-            {console.log(data)}
-            loaded data
+            <button onClick={newOrder}>new order</button>
+            {loading === true ? <div>loading...</div> : ''}
+            {data ? (
+                <>
+                {console.log(data)}
+                loaded data
+                </>
+            ):''}
             </>
-        ):''}
-        </>
-      )
+        )
 
-}
+    }

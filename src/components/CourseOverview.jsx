dc918021895351c61a7012089d788a176e741376
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-07-11 02:36:51 -0700

    Update CourseOverview.jsx

--- src/components/CourseOverview.jsx
+++ src/components/CourseOverview.jsx
@@ -1,22 +1,76 @@
 import * as React from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import useAuth, { User } from "../hooks/useAuth";
 import { navigate } from 'gatsby';
-import CourseOverview from '../components/CourseOverview';
 
 
-export default function GetHome() {
+
+export default function CourseOverview() {
+  const [loadingCourses, setLoadingCourses] = useState(true)
+  const [genericError, setGenericError] = useState(false)
+  const [courseData, setCourseData] = useState([])
+  const [noCourses, setNoCourses] = useState(true)
   useEffect(() => {
-    if (window.location.href.indexOf("dashboard") === -1) {
-      window.location.reload();
+    async function fetchMyAPI() {
+      const controller = new AbortController();
+      const timeoutId = setTimeout(() => controller.abort(), 15000); 
+      try {
+        const request = await fetch('https://portal.revrevdev.xyz/wp-json/rev-process/learn', {
+          method: 'POST',
+          signal: controller.signal,
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        const courses = (await request.json());
+        console.log(courses)
+        if (intent !== false) {
+          setNoCourses(false)
+          setCourseData(courses)
+        }
+       setLoadingCourses(false)
+      } catch (error) {
+        setGenericError(true)
+        setLoadingCourses(false)
+      }
+      clearTimeout(timeoutId);
     }
-  });
-  const { loggedIn, loading, user } = useAuth();
-  const { id } = user;
+    fetchMyAPI()
+  }, []);
+
 
   return (
-    <div className='sub-list'>
-        <CourseOverview/>
-    </div>
+  <>
+  {loadingCourses === true ? (
+    'loading'
+  ): (
+    <>
+
+      {genericError === true ? (
+        'generic error'
+      ):null}
+
+      {noCourses === true ? (
+        'no courses'
+      ): (
+        <>
+          {courseData.length > 0 ? (
+              <>
+              {console.log(aa, courseData)}
+              <p>plenty of data here</p>
+              </>
+          ):('no course data')}
+
+
+        </>
+      )}
+
+
+    </>
+  )}
+
+
+  </>
   );
 }

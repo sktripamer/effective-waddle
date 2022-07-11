24b2c58f37e72a03b064e53bf9022afcaef702e5
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-07-11 05:17:46 -0700

    Update CourseOverview.jsx

--- src/components/CourseOverview.jsx
+++ src/components/CourseOverview.jsx
@@ -10,6 +10,7 @@ export default function CourseOverview() {
   const [genericError, setGenericError] = useState(false)
   const [courseData, setCourseData] = useState([])
   const [noCourses, setNoCourses] = useState(true)
+  const [activeTag, setActiveTag] = useState("in-progress")
   useEffect(() => {
     async function fetchMyAPI() {
       const controller = new AbortController();
@@ -41,6 +42,40 @@ export default function CourseOverview() {
   }, []);
 
 
+  function slugify(text) {
+    return text
+      .toString()                           // Cast to string (optional)
+      .normalize('NFKD')            // The normalize() using NFKD method returns the Unicode Normalization Form of a given string.
+      .toLowerCase()                  // Convert the string to lowercase letters
+      .trim()    
+      .replace('×', 'x')
+      .replace(/\./g, '-')
+      .replace(/\//ig, '-') 
+      .replace(/\s+/g, '-')            // Replace spaces with -
+      .replace(/[^\w\-]+/g, '')     // Remove all non-word chars
+      .replace(/\-\-+/g, '-');        // Replace multiple - with single -
+  }
+
+  const restartAnimation = () => {
+    for (const animation of document.getElementsByClassName('course-slider-cont')[0].getAnimations()) {
+        if (document.getElementsByClassName('course-slider-cont')[0].contains((animation.effect).target)) {
+          animation.cancel();
+          animation.play();
+        }
+      }
+}
+  const onClick1 = () => {
+    setActiveTag("in-progress")
+    restartAnimation()
+  }
+  const onClick2 = () => {
+      setActiveTag("completed")
+      restartAnimation()
+  }
+  const onClick3 = () => {
+      setActiveTag("all")
+      restartAnimation()
+  }
   return (
   <>
   {loadingCourses === true ? (
@@ -55,16 +90,46 @@ export default function CourseOverview() {
       {noCourses === true ? (
         'no courses'
       ): (
-        <>
+        <div class={`ownedcourse-slider ${activeTag}`}>
+
+          <div class={`tag-cont ${activeTag.replace("'", '').replace(' ', '')}`}>
+              <div onClick={onClick1} class='ind-tag tag-in-progress'>In Progress</div><div onClick={onClick2} class='ind-tag tag-completed'>Completed</div><div onClick={onClick3} class='ind-tag tag-all'>All Courses</div>
+          </div>
           {courseData.length > 0 ? (
               <>
-              {console.log('aa', courseData)}
-              <p>plenty of data here</p>
+              <div class='course-slider-cont'>
+                {courseData.map((el) => 
+                  <>
+                  <div class={`coursebox-cont ${el.data.status}`}>
+                    <div class='coursebox-top'>
+                      <div class='coursebox-image-cont'>
+                        <div style={Object.assign({'background-image': `url(${el.image})` })} class='courseboximg'></div>
+                      </div>
+                      <div className='course-info'>{el.title}</div>
+                      <div className='course-desc'>{el.description}</div>
+                    </div>
+                    <div class='coursebox-bot'>
+                      {el.data.status === 'not_started' ? (
+                          <a class='course-btn btn-start' href={`https://portal.revrevdev.xyz/courses/${slugify(el.title)}`} target="_blank" rel="noreferrer noopener">Start Course</a>
+                      ) : ('')}
+                          {el.data.status === 'in_progress' ? (
+                          <a class='course-btn btn-continue' href={`https://portal.revrevdev.xyz/courses/${slugify(el.title)}`} target="_blank" rel="noreferrer noopener">Continue Course</a>
+                      ) : ('')}
+                          {el.data.status === 'completed' ? (
+                          <a class='course-btn btn-finished' href={`https://portal.revrevdev.xyz/courses/${slugify(el.title)}`} target="_blank" rel="noreferrer noopener">Review Course</a>
+                      ) : ('')}
+                      <div class="progressbar">
+                        <div style={Object.assign({width: `${Math.round((el.data.completed / el.data.total) * 100)}%` })} class="bar-filler">{`${Math.round((el.data.completed / el.data.total) * 100)}%`}</div>
+                      </div>
+                    </div>
+                  </div>
+                  </>
+                )}
+              </div>
               </>
           ):('no course data')}
 
-
-        </>
+        </div>
       )}
 
 

0084d989d80c2beb98ab9bd0999223490ee03cf4
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-07-12 22:33:22 -0700

    Update GetDashboard.jsx

--- src/components/GetDashboard.jsx
+++ src/components/GetDashboard.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import { navigate } from 'gatsby';
 import CourseOverview from '../components/CourseOverview';

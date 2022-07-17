5797e4fe8e6d5ececd1818fffcf34f5bde0d16ba
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-07-17 09:56:21 -0700

    Update RevRevCourseSmallList.jsx

--- src/components/RevRevCourseSmallList.jsx
+++ src/components/RevRevCourseSmallList.jsx
@@ -102,7 +102,7 @@ export default function RevRevCourseSmallList({preReveal, changeCart, courseData
        setLocal('cart', JSON.stringify(cartModifier))
      
      }
-    
+    console.log(courseData)
 
     return (
         <>

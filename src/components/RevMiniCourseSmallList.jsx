c7853cc365966dae602e0358a1ed5ddc346a6ff7
sktripamer <42321951+sktripamer@users.noreply.github.com>
2022-07-18 08:05:17 -0700

    Update RevMiniCourseSmallList.jsx

--- src/components/RevMiniCourseSmallList.jsx
+++ src/components/RevMiniCourseSmallList.jsx
@@ -1,18 +1,14 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
-import DOMPurify from 'dompurify';
 import Layout from "../components/Layout";
+import CourseOverview from '../components/CourseOverview';
 
 
-
-export default function RevMiniCourseSmallList({preReveal, changeCart, courseData, changeCartVis}) {
+export default function RevMiniCourseSmallList({preReveal, prePreReveal, changeCart, courseData, changeCartVis, changeDetails}) {
 
     const [addedToCarts, setAddedToCarts] = useState([])
-    const [highestVal, setHighestVal] = useState(null)
-    const sanitizedData = (sendData) => ({
-        __html: DOMPurify.sanitize(sendData)
-      })
     useEffect(() => {
+        document.addEventListener("itemInserted", localStorageSetHandler, false);
         let tempCart = function() {
             try {
             return JSON.parse(localStorage.cart)
@@ -20,30 +16,41 @@ export default function RevMiniCourseSmallList({preReveal, changeCart, courseDat
         }
         let addedToCart = []
         tempCart().forEach((cartitem, index) => {
-           courseData.forEach((propitem, index2) => {
-                if (cartitem.ID === propitem.cart) {
-                    addedToCart.push(index2)
-                }
-            })
+                    addedToCart.push(cartitem.ID)
 
         })
         console.log(addedToCart)
         setAddedToCarts(addedToCart);
-        let highest = -1;
-        courseData.forEach((propitem, index) => {
-            if (parseInt(propitem.featured) > highest ) {
-                highest = parseInt(propitem.featured)
-            }
-        })
-        setHighestVal(highest)
-        
+
     }, []);
+
+    const localStorageSetHandler = () => {
+        let tempCart = function() {
+            try {
+            return JSON.parse(localStorage.cart)
+            } catch {return []}      
+        }
+        let addedToCart = []
+        tempCart().forEach((cartitem, index) => {
+                    addedToCart.push(cartitem.ID)
+
+        })
+        console.log(addedToCart)
+        setAddedToCarts(addedToCart);
+    }
 
     const buyNow = (cart) => {
         console.log('herea')
        changeCart([cart])
         preReveal()
     }
+
+    const viewMore = (details) => {
+        console.log('herea')
+       changeDetails(details)
+        prePreReveal()
+    }
+
     const SimpleCart = (e) => {
         let dbID;
          let varName;
@@ -97,7 +104,7 @@ export default function RevMiniCourseSmallList({preReveal, changeCart, courseDat
         //if no duplicate cart item is found to already exist in localstorage cart array, simply add it to the array.
         if (cartItemFound === false && cartObj.quantity !== 0) {
             cartModifier.push(cartObj)
-            const newCart = parseInt(e.target.dataset.idx)
+            const newCart = parseInt(e.target.dataset.cartid)
               
               const updatedCartsArray = [...addedToCarts, newCart];
                 console.log(updatedCartsArray)
@@ -114,14 +121,18 @@ export default function RevMiniCourseSmallList({preReveal, changeCart, courseDat
        setLocal('cart', JSON.stringify(cartModifier))
      
      }
-    
+    console.log(courseData)
 
     return (
         <>
 
-           
-  
-            {courseData.map((el, index) =>
+        {courseData?.owned?.length > 0 ? (
+          <CourseOverview sectionImg={'revminicourseimg'} sectionTitle={'All My Mini Courses'} sectionDescription={'Continue where you left off on your unlocked Mini Courses'} cat='Mini Course' courseData={courseData.owned} />
+         ) :
+          ( null)}
+
+
+        {courseData?.available?.map((el, index) =>
             <>
             
                {el.cat === "Mini Course" ? (<>
@@ -166,66 +177,56 @@ export default function RevMiniCourseSmallList({preReveal, changeCart, courseDat
             
             )}
 
-            <div class='smallcourse-head'>Mini Courses</div>
+
+
+            <div class='smallcourse-head'>Locked Mini Courses</div>
             <div class='course-display-cont'>
-            {courseData.map((el, index) =>
+            {courseData?.available?.map((el, index) =>
             <>
-            
                {el.cat === "Mini Course" ? (<>
-                {parseInt(el.featured) === highestVal ? (
-                  <></>
-                ):(
+                <div class='courseboxfullcont-cont'>
+                {el.tag === 'new' ? <div class='selltag-new'></div> : ''}
+                <div class={`courseboxbuy-cont ind-${index}`}>
                    
-                    <div class='courseboxfullcont-cont'>
-                    {el.tag === 'new' ? <div class='selltag-new'></div> : ''}
-                    <div class={`courseboxbuy-cont ind-${index}`}>
-                       
-                        <div class={addedToCarts.includes(index) === true ? "courseboxall-cont filled" : 'courseboxall-cont' }>
-                            <div class='coursebox-top'>
-                                <div class='coursebox-image-cont'>
-                                    <div style={Object.assign({'background-image': `url(${el.image})` })} class='courseboximg'></div>
-                                </div>
-                            </div>
-                            <div class='coursebox-bot'>
-                                <div className='course-infobuy'>{el.title}</div>
-                                
-                                {addedToCarts.includes(index) === true ? (
-                                    <div onClick={() => changeCartVis(true)} className='course-addedtocart'>View Cart</div>
-                                ): (
-                                    <div className='course-infopopup'>Get Access</div>
-                                )}
-                              
-                               
+                    <div class={addedToCarts.includes(el.cart) === true ? "courseboxall-cont filled" : 'courseboxall-cont' }>
+                        <div class='coursebox-top'>
+                            <div class='coursebox-image-cont'>
+                                <div style={Object.assign({'background-image': `url(${el.image})` })} class='courseboximg'></div>
                             </div>
                         </div>
-                        <div class='course-popup'>
-                                    <div class='top-partpop'>
-                                        <div class='popup-description'>{el.description}</div>
-                                        <div class='popup-price'>${el.price}</div>
-                                    </div>
-                                    <div class='popup-buttons'>
-                                        <button onClick={SimpleCart} data-idx={index} data-img={el.image} data-cartid={el.cart} data-price={el.price} data-name={el.title} class='popup-quickadd'>Quick Add</button>
-                                        <button onClick={() => buyNow({i: el.cart, q: 1, p: el.price, t: el.price})} class='popup-buynow'>Buy Me!</button>
-                                    </div>
+                        <div class='coursebox-bot'>
+                            <div className='course-infobuy'>{el.title}</div>
+                            
+                            {addedToCarts.includes(el.cart) === true ? (
+                                <div onClick={() => changeCartVis(true)} className='course-addedtocart'>View Cart</div>
+                            ): (
+                                <div className='course-infopopup'>Get Access</div>
+                            )}
+                          
+                           
+                        </div>
+                    </div>
+                    <div class='course-popup'>
+                                <div class='top-partpop'>
+                                    <div class='popup-description'>{el.description}</div>
+                                    <button onClick={() => viewMore({sub: el.sub, img: el.square, desc: el.long, price:el.price, title: el.title, cart: {i: el.cart, q: 1, p: el.price, t: el.price}})} class='learn-more-btn'>Learn More</button>
+                                    <div class='popup-price'>${el.price}</div>
                                 </div>
-                    </div>
-                    </div>
-                   
-                )}
-
-
-
-
-
+                                <div class='popup-buttons'>
+                                    <button onClick={SimpleCart} data-idx={index} data-img={el.image} data-cartid={el.cart} data-price={el.price} data-name={el.title} class='popup-quickadd'>Quick Add</button>
+                                    <button onClick={() => buyNow({i: el.cart, q: 1, p: el.price, t: el.price})} class='popup-buynow'>Buy Me!</button>
+                                </div>
+                            </div>
+                </div>
+                </div>
+               
                </>)
                
                :(<></>)}
-                  
             </>
-            
             )}
+        
         </div>
-     
         </>
     )
 
